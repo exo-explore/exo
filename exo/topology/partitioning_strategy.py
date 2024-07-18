fa9d4169557492986ff663936a4a3110a2430374
Alex Cheema <alexcheema123@gmail.com>
2024-07-18 16:20:01 -0700

    rm unused imports

--- exo/topology/partitioning_strategy.py
+++ exo/topology/partitioning_strategy.py
@@ -1,8 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List
 from dataclasses import dataclass
-from exo.inference.shard import Shard
-from exo.networking.peer_handle import PeerHandle
 from .topology import Topology
 
 # Partitions shard-space into pieces of contiguous shards, represented by floating point range [start, end) between 0 and 1
