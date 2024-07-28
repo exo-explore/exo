666b1c83ee0954ae35f3f7197126db4c15d296a6
Anchen <li.anchen.au@gmail.com>
2024-07-28 12:46:38 +1000

    refactor(mlx): model sharding and add deepseek v2 support

--- exo/api/chatgpt_api.py
+++ exo/api/chatgpt_api.py
@@ -54,6 +54,12 @@ shard_mappings = {
       model_id="mlx-community/Mistral-Large-Instruct-2407-4bit", start_layer=0, end_layer=0, n_layers=88
     ),
   },
+  ### deepseek v2
+  "deepseek-coder-v2-lite": {
+    "MLXDynamicShardInferenceEngine": Shard(
+      model_id="mlx-community/DeepSeek-Coder-V2-Lite-Instruct-4bit-mlx", start_layer=0, end_layer=0, n_layers=27
+    ),
+  },
 }
 
 
