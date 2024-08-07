2975929d35c963b0e06c815d6b18b4625fa65eef
risingsunomi <vincentcastro@gmail.com>
2024-08-07 15:10:44 -0800

    fixing hugging face sharded class

--- exo/inference/pytorch/model/hf.py
+++ exo/inference/pytorch/model/hf.py
@@ -4,14 +4,14 @@ from transformers import AutoModelForCausalLM, DynamicCache
 from exo.inference.shard import Shard
 
 class ShardedHuggingFaceModel(nn.Module):
-    def __init__(self, model_name: str, shard: Shard):
+    def __init__(self, shard: Shard):
         super(ShardedHuggingFaceModel, self).__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.shard = shard
 
         # Load the model
         self.full_model = AutoModelForCausalLM.from_pretrained(
-            model_name,
+            shard.model_id,
             torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
             device_map="auto"
         )
