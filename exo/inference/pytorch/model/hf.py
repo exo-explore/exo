989f29e8ca9e4ee1bceaf37253821b8f5cf2f49e
risingsunomi <vincentcastro@gmail.com>
2024-08-08 22:11:19 -0800

    reshaping for norm layer call at end

--- exo/inference/pytorch/model/hf.py
+++ exo/inference/pytorch/model/hf.py
@@ -97,7 +97,7 @@ class ShardedHuggingFaceModel(torch.nn.Module):
             
             # Forward pass through the layer
             if DEBUG >= 2:
-                print(f"\nhidden_states {hidden_states}")
+                print(f"\n[layer {i}] hidden_states {hidden_states}")
 
             layer_outputs = layer(
                 hidden_states,
@@ -107,7 +107,7 @@ class ShardedHuggingFaceModel(torch.nn.Module):
             )
 
             if DEBUG >= 2:
-                print(f"\nlayer_outputs[0]: {layer_outputs[0]}")
+                print(f"\n[layer {i}] layer_outputs: {layer_outputs[0]}")
             
             hidden_states = layer_outputs[0]
             # new_past_key_values.append(layer_outputs[1])
