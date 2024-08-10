f54990a121db9a52c8775cefad9dd1cb2c7aeab8
risingsunomi <vincentcastro@gmail.com>
2024-08-10 12:33:19 -0800

    working on eot error

--- exo/inference/pytorch/model/hf.py
+++ exo/inference/pytorch/model/hf.py
@@ -74,7 +74,10 @@ class ShardedHuggingFaceModel(torch.nn.Module):
                 print(f"hidden_states: {hidden_states}")
                 print(f"hidden_states.size(): {hidden_states.size()}")
 
-            batch_size, seq_len = input_data.size()
+            if hidden_states.size == 2:
+                batch_size, seq_len = hidden_states.size()
+            else:
+                batch_size, seq_len = input_data.size()
             position_ids = torch.arange(seq_len, dtype=torch.long, device=self.device).unsqueeze(0).expand(batch_size, -1)
 
             position_embeddings = self.full_model.model.rotary_emb(
