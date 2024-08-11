2ca9689b014297776b77c44505d05c49d5c0d83e
risingsunomi <vincentcastro@gmail.com>
2024-08-10 18:01:07 -0800

    float long issue

--- exo/inference/pytorch/model/utils.py
+++ exo/inference/pytorch/model/utils.py
@@ -83,7 +83,7 @@ from torch.nn import functional as F
 
 #     return sampled_token.squeeze()
 
-def sample_logits(ogits, temperature=1.0, top_k=0, top_p=1.0, alpha_f=0.0, alpha_p=0.0):
+def sample_logits(logits, temperature=1.0, top_k=0, top_p=1.0, alpha_f=0.0, alpha_p=0.0):
     """
     Sample tokens from logits using temperature, top-k, and top-p (nucleus) sampling.
 
