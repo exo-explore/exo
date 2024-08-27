f485b94813e868fadfb68944220c3eeb7d6a5abf
Baye Dieng <bayedieng98@gmail.com>
2024-08-27 12:02:21 +0000

    initial commit

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ install_requires = [
   "tqdm==4.66.4",
   "transformers==4.43.3",
   "uuid==1.30",
+  "llama-cpp-python==0.2.89",
   "tinygrad @ git+https://github.com/tinygrad/tinygrad.git@639af3f823cf242a1945dc24183e52a9df0af2b7",
 ]
 
