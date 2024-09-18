98eaf69440c1ac6767b5a0a881f233b35407f735
Varshith <varshith.bathini@sprinklr.com>
2024-09-19 04:16:05 +0530

    testcases fixed

--- exo/inference/mlx/test_sharded_llava.py
+++ exo/inference/mlx/test_sharded_llava.py
@@ -1,64 +1,64 @@
-import codecs
 import asyncio
-import requests
-from PIL import Image
-from io import BytesIO
+import unittest
 
-import mlx.core as mx
-from mlx_lm.models.base import KVCache
-
-from exo.inference.mlx.sharded_model import StatefulShardedModel
-from exo.inference.mlx.sharded_utils import load_shard
 from exo.inference.shard import Shard
+from exo.download.hf.hf_shard_download import HFShardDownloader
+from exo.inference.mlx.sharded_inference_engine import MLXDynamicShardInferenceEngine
 
-shard_full = Shard("llava", 0, 31, 32)
-shard1 = Shard("llava", 0, 12, 32)
-shard2 = Shard("llava", 13, 31, 32)
 
-model_path = "llava-hf/llava-1.5-7b-hf"
+class LlavaShardTest(unittest.TestCase):
+    def setUp(self):
+        self.inference_engine = MLXDynamicShardInferenceEngine(HFShardDownloader())
+        self.inference_engine1 = MLXDynamicShardInferenceEngine(HFShardDownloader())
+        model_id = "llava-hf/llava-1.5-7b-hf"
+        self.full_shard = Shard(model_id=model_id, start_layer=0, end_layer=31, n_layers=32)
+        self.shard1 = Shard(model_id=model_id, start_layer=0, end_layer=12,
+                            n_layers=32)
+        self.shard2 = Shard(model_id=model_id, start_layer=13, end_layer=31,
+                            n_layers=32)
+        self.prompt = "USER: <image>\nWhat are these?\nASSISTANT:"
+        self.img = "http://images.cocodataset.org/val2017/000000039769.jpg"
+        self.max_tokens = 30
 
-full_model_shard, full_processor = asyncio.run(load_shard(model_path, shard=shard_full))
-model_shard1, processor1 = asyncio.run(load_shard(model_path, shard=shard1))
-model_shard2, processor2 = asyncio.run(load_shard(model_path, shard=shard2))
+    async def _test_full_shard(self):
+        resp, inference_state, _ = await self.inference_engine.infer_prompt("A", shard=self.full_shard,
+                                                                            prompt=self.prompt, image_str=self.img)
+        generated_tokens = [resp.item()]
+        for _ in range(self.max_tokens):
+            resp, inference_state, _ = await self.inference_engine.infer_tensor(
+                "A",
+                shard=self.full_shard,
+                input_data=resp,
+                inference_state=inference_state,
+            )
+            generated_tokens.append(resp.item())
 
-full = StatefulShardedModel(shard_full, full_model_shard)
-m1 = StatefulShardedModel(shard1, model_shard1)
-m2 = StatefulShardedModel(shard2, model_shard2)
+        print("Response:\n", self.inference_engine.tokenizer.decode(generated_tokens))
+        return generated_tokens
 
-PROMPT = "USER: <image>\nWhat are these?\nASSISTANT:"
-IMAGE_FILE = "http://images.cocodataset.org/val2017/000000039769.jpg"
-response = requests.get(IMAGE_FILE)
-img = Image.open(BytesIO(response.content))
-prompt = codecs.decode(PROMPT, "unicode_escape")
-inputs = full_processor(prompt, img, return_tensors="np")
-pixel_values = mx.array(inputs["pixel_values"])
-input_ids = mx.array(inputs["input_ids"])
+    async def _test_partial_shard(self):
+        resp1, inference_state_1, _ = await self.inference_engine.infer_prompt("B", shard=self.shard1,
+                                                                               prompt=self.prompt, image_str=self.img)
+        resp2, inference_state_2, _ = await self.inference_engine1.infer_tensor("B", shard=self.shard2,
+                                                                                input_data=resp1,
+                                                                                inference_state=inference_state_1)
+        generated_tokens = [resp2.item()]
+        for _ in range(self.max_tokens):
+            resp1, inference_state_1, _ = await self.inference_engine.infer_tensor("B", shard=self.shard1,
+                                                                                   input_data=resp2,
+                                                                                   inference_state=inference_state_2)
+            resp2, inference_state_2, _ = await self.inference_engine1.infer_tensor("B" , shard=self.shard2,
+                                                                                    input_data=resp1,
+                                                                                    inference_state=inference_state_1)
+            generated_tokens.append(resp2.item())
+        print("Response:\n", self.inference_engine.tokenizer.decode(generated_tokens))
+        return generated_tokens
 
-print(prompt)
-y = full.step("full", input_ids, pixel_values, temp=0)
-full_generated_tokens = [y.item()]
+    def test_inference_engine(self):
+        full_generated_tokens = asyncio.run(self._test_full_shard())
+        sharded_generated_tokens = asyncio.run(self._test_partial_shard())
+        assert full_generated_tokens == sharded_generated_tokens
 
-for _ in range(13):
-  y = full.step("full", y, temp=0)
-  full_generated_tokens.append(y.item())
 
-full_response = full_processor.tokenizer.decode(full_generated_tokens)
-print("full response:", full_response)
-
-inputs = processor1(prompt, img, return_tensors="np")
-pixel_values = mx.array(inputs["pixel_values"])
-input_ids = mx.array(inputs["input_ids"])
-
-y = m1.step("shard", input_ids, pixel_values, temp=0)
-y = m2.step("shard", y, temp=0)
-full_generated_tokens = [y.item()]
-
-for _ in range(13):
-  y = m1.step("shard", y, temp=0)
-  y = m2.step("shard", y, temp=0)
-  full_generated_tokens.append(y.item())
-
-sharded_response = processor2.tokenizer.decode(full_generated_tokens)
-print("sharded response:", sharded_response)
-
-assert full_response == sharded_response
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
