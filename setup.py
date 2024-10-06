b44f6e975f4b77642fba46a5fde601499c0bb52b
risingsunomi <vincentcastro@gmail.com>
2024-10-06 13:08:14 -0800

    updating pytorch requirement

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ install_requires = [
   "transformers==4.43.3",
   "uuid==1.30",
   "tinygrad @ git+https://github.com/tinygrad/tinygrad.git@232edcfd4f8b388807c64fb1817a7668ce27cbad",
-  "torch==2.4.0+cu124",
+  "torch==2.4.0",
   "accelerate"
 ]
 
