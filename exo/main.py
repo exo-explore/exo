b2928c04e661efa5f5e5355ebf1ff9b6893c60f8
thenatlog <logan.w.omalley@gmail.com>
2024-10-07 18:43:21 -0400

    update main flag

--- exo/main.py
+++ exo/main.py
@@ -22,6 +22,9 @@ from exo.inference.tokenizers import resolve_tokenizer
 from exo.orchestration.node import Node
 from exo.models import model_base_shards
 from exo.viz.topology_viz import TopologyViz
+from exo.topology.available_memory_weighted_partitioning_strategy import AvailableMemoryWeightedPartitioningStrategy
+from exo.topology.ring_memory_weighted_partitioning_strategy import RingMemoryWeightedPartitioningStrategy
+
 
 # parse args
 parser = argparse.ArgumentParser(description="Initialize GRPC Discovery")
@@ -47,6 +50,13 @@ parser.add_argument("--run-model", type=str, help="Specify a model to run direct
 parser.add_argument("--prompt", type=str, help="Prompt for the model when using --run-model", default="Who are you?")
 parser.add_argument("--tailscale-api-key", type=str, default=None, help="Tailscale API key")
 parser.add_argument("--tailnet-name", type=str, default=None, help="Tailnet name")
+parser.add_argument(
+    "--partitioning-strategy",
+    type=str,
+    choices=["available_memory", "ring_memory"],
+    default="available_memory",
+    help="Partitioning strategy to use"
+)
 args = parser.parse_args()
 
 print_yellow_exo()
@@ -79,14 +89,22 @@ if args.discovery_module == "udp":
 elif args.discovery_module == "tailscale":
   discovery = TailscaleDiscovery(args.node_id, args.node_port, lambda peer_id, address, device_capabilities: GRPCPeerHandle(peer_id, address, device_capabilities), discovery_timeout=args.discovery_timeout, tailscale_api_key=args.tailscale_api_key, tailnet=args.tailnet_name)
 topology_viz = TopologyViz(chatgpt_api_endpoints=chatgpt_api_endpoints, web_chat_urls=web_chat_urls) if not args.disable_tui else None
+
+if args.partitioning_strategy == "available_memory":
+    partitioning_strategy = AvailableMemoryWeightedPartitioningStrategy()
+elif args.partitioning_strategy == "ring_memory":
+    partitioning_strategy = RingMemoryWeightedPartitioningStrategy()
+else:
+    partitioning_strategy = None
+
 node = StandardNode(
-  args.node_id,
-  None,
-  inference_engine,
-  discovery,
-  partitioning_strategy=RingMemoryWeightedPartitioningStrategy(),
-  max_generate_tokens=args.max_generate_tokens,
-  topology_viz=topology_viz
+    args.node_id,
+    None,
+    inference_engine,
+    discovery,
+    partitioning_strategy=partitioning_strategy,
+    max_generate_tokens=args.max_generate_tokens,
+    topology_viz=topology_viz
 )
 server = GRPCServer(node, args.node_host, args.node_port)
 node.server = server
