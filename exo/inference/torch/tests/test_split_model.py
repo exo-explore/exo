47be250dbe743d3624f3ae2195ed50e9a2704373
risingsunomi <vincentcastro@gmail.com>
2024-10-13 04:58:25 -0800

    working on split model, moving to server for more vram

--- exo/inference/torch/tests/test_split_model.py
+++ exo/inference/torch/tests/test_split_model.py
@@ -3,69 +3,150 @@ Testing of loading model by layer
 """
 import asyncio
 import re
+import json
+import os
+from pathlib import Path
+from typing import Optional
 
-from exo.download.hf.hf_helpers import get_weight_map
+from exo.download.hf.hf_helpers import (
+  get_weight_map,
+  download_repo_files
+)
 from exo.download.hf.hf_shard_download import HFShardDownloader
 from exo.inference.shard import Shard
 
-from typing import Optional, Union, Tuple
-
-from transformers import AutoModel
+from transformers import AutoModelForCausalLM
 
 async def load_model(
   repo_id: str,
-  shard: Shard
-) -> Optional[AutoModel]:
+  shard: Shard,
+  model_path: Path,
+  weight_map: Optional[dict]
+) -> Optional[AutoModelForCausalLM]:
   """
   load model by layer and safetensors
+  return causal llm automodel with only requested layers, if weight maps
+  if no weight map, return and load the whole model
   """
-
-  shard_downloader = HFShardDownloader()
-  model_path = await shard_downloader.ensure_shard(shard)
-  weight_map = await get_weight_map(repo_id)
-
+  print("load_model called")
   if weight_map:
+    layer_weight_map = {}
+    skip_layers = []
+
     for wname, wtensor in weight_map.items():
       # get layer number
-      layer_rgx = r'^model\.layers\.(\d+)\.(\w+)\.(\w+)$'
+      layer_rgx = r'^model\.layers\.(\d+)\.*'
       layer_found = re.findall(layer_rgx, wname)
+      print(f"wname: {wname}")
       if layer_found:
-        try:
-          layer_idx = int(layer_found[0][0])
-          print(f"layer_idx: {layer_idx}")
-          if shard.start_layer <= layer_idx <= shard.end_layer:
-            print(f"wtensor: {wtensor}")
+        print(f"layer_found: {layer_found}")
+        # slice up layer map to start and end layers
+        # from shard
+        layer_idx = int(layer_found[0])
+        if shard.start_layer <= layer_idx <= shard.end_layer:
+          layer_weight_map[wname] = wtensor
+        else:
+          skip_layers.append(wname)
+          print(f"SKIPPING LAYER {layer_idx}")
 
-            # move to local .tmp folder that can be removed later
-            # check if files not already there, if there, reuse
-            # create automodel with rest of layers
-            # lm_head needed at end
-        except Exception as err:
-          print(f"err: {err}")
+      if wname not in skip_layers:
+        print(f"adding non-layer: {wname}")
+        layer_weight_map[wname] = wtensor
 
-async def test_split_model(model_id: str, n_layers: int):
+    # will manipulate current model.safetensors.index.json
+    # but set back at end of inference
+    print(layer_weight_map)
+
+    # rewrite model.safetensors.index.json
+    try:
+      model_st_snapshot = model_path/"model.safetensors.index.json"
+      # call download repo files again to reload original safetensors json
+      os.remove(model_st_snapshot)
+
+      await download_repo_files(
+        repo_id=shard.model_id,
+        revision="main",
+        allow_patterns="model.safetensors.index.json")
+
+      mst_json = {}
+      with open(model_st_snapshot, "r") as mst_file:
+        mst_json = json.load(mst_file)
+
+        mst_json["weight_map"] = layer_weight_map
+
+        print(f"mst_json: {json.dumps(mst_json, indent=4)}")
+
+      with open(model_st_snapshot, "w") as mst_file:
+        json.dump(mst_json, mst_file, indent=4)
+        print(f"{model_st_snapshot} rewritten with {shard.n_layers} weights")
+    except Exception as err:
+      print(f"err: {err}")
+      raise
+
+  else:
+    print("weight_map not found, loading whole model")
+
+  # load model with layer edits
+  # or whole model if no weight_map
+  shard_model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    device_map="auto",
+    offload_buffers=True
+  )
+
+  return shard_model
+
+
+async def test_split_model(
+  model_id: str,
+  start_layer: int,
+  end_layer: int,
+  n_layers: int
+):
   """
   Test to load split models
   """
 
   shard = Shard(
     model_id=model_id,
-    start_layer=0,
-    end_layer=n_layers-1,
+    start_layer=start_layer,
+    end_layer=end_layer-1,
     n_layers=n_layers
   )
 
+  print(f"loading shard: {shard}")
+  shard_downloader = HFShardDownloader()
+  model_path = await shard_downloader.ensure_shard(shard)
+  weight_map = await get_weight_map(model_id)
+
   await load_model(
     model_id,
-    shard
+    shard,
+    model_path,
+    weight_map
   )
 
 if __name__ == "__main__":
+  #Qwen/Qwen2.5-3B
   try:
-    print("\n-------- Test unsloth/Meta-Llama-3.1-8B-Instruct ----------\n")
+    print("\n-------- Test Qwen/Qwen2.5-3B ----------\n")
     asyncio.run(test_split_model(
-      "unsloth/Meta-Llama-3.1-8B-Instruct",
-      32
+      "Qwen/Qwen2.5-3B",
+      0,
+      1,
+      36
     ))
   except Exception as err:
     print(f"\n\n !!!!!!!!!!! meta-llama/Llama-3.2-1B-Instruct TEST FAILED \n{err}\n")
+
+  # unsloth/Meta-Llama-3.1-8B-Instruct
+    #try:
+    #  print("\n-------- Test unsloth/Meta-Llama-3.1-8B-Instruct ----------\n")
+    #  asyncio.run(test_split_model(
+    #    "unsloth/Meta-Llama-3.1-8B-Instruct",
+    #    0,
+    #    1,
+    #    32
+    #  ))
+    #except Exception as err:
+    #  print(f"\n\n !!!!!!!!!!! meta-llama/Llama-3.2-1B-Instruct TEST FAILED \n{err}\n")
