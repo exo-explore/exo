1a6b89fbcde6b1822db024e56c945205771645d4
Sebastian Nicolas <snicolas432@gmail.com>
2024-10-14 23:28:39 -0400

    fixed both mlx and tinygrad benchmarks by adding synchronization calls after evaluating/realizing the matmuls

--- exo/inference/mlx/benchmark.py
+++ exo/inference/mlx/benchmark.py
@@ -3,32 +3,30 @@
 import time
 import mlx.core as mx
 
-def measure_runtime(fn, *args, **kwargs):
+def _benchmark_matmul(n, dtype):
+    a = mx.random.uniform(shape=(n, n), dtype=dtype)
+    b = mx.random.uniform(shape=(n, n), dtype=dtype)
+
     # warmup
     for _ in range(5):
-        x = mx.eval(fn(*args, **kwargs))
+        x = mx.eval(a@b)
+        mx.synchronize()
 
     num_iters = 100
     tic = time.perf_counter()
     for _ in range(num_iters):
-        x = mx.eval(fn(*args, **kwargs))
+        x = mx.eval(a@b)
+        mx.synchronize()
     toc = time.perf_counter()
 
     sec = (toc - tic) / num_iters
-    return sec
-
-def time_matmul(n=1024, dtype=mx.float32, device=mx.gpu):
-    mx.random.seed(333)
-    a = mx.random.uniform(shape=(n, n), dtype=dtype, stream=device)
-    b = mx.random.uniform(shape=(n, n), dtype=dtype, stream=device)
-    mx.eval(a, b)
-    sec = measure_runtime(mx.matmul, a, b, stream=device)
     tflops = (2 * (n ** 3) / sec) / 1e12
     return tflops
 
 def mlx_benchmark_tflops():
-    f32 = round(time_matmul(n=1024), 2)
-    f16 = round(time_matmul(n=1024, dtype=mx.float16), 2)
+    n = 2048
+    f32 = round(_benchmark_matmul(n, dtype=mx.float32), 2)
+    f16 = round(_benchmark_matmul(n, dtype=mx.float16), 2)
     return (f32, f16, 0.0)
 
 if __name__ == "__main__":
--- exo/inference/tinygrad/benchmark.py
+++ exo/inference/tinygrad/benchmark.py
@@ -1,34 +1,31 @@
 import time
 from tinygrad import Tensor
-from tinygrad import dtypes
+from tinygrad import Device, dtypes
 
-def measure_runtime(fn, *args, **kwargs):
+def _benchmark_matmul(n, dtype):
+    a = Tensor.rand(n, n, dtype=dtype)
+    b = Tensor.rand(n, n, dtype=dtype)
+
     # warmup
     for _ in range(5):
-        fn(*args, **kwargs).realize()
-
+        (a@b).realize()
+        Device.default.synchronize()
+    
     num_iters = 100
     tic = time.perf_counter()
     for _ in range(num_iters):
-        fn(*args, **kwargs).realize()
+        (a@b).realize()
+        Device.default.synchronize()
     toc = time.perf_counter()
 
     sec = (toc - tic) / num_iters
-    return sec
-
-def time_matmul(n=1024, dtype=dtypes.float32):
-    Tensor.manual_seed(333)
-    a = Tensor.rand(n, n, dtype=dtype)
-    b = Tensor.rand(n, n, dtype=dtype)
-    a.realize()
-    b.realize()
-    sec = measure_runtime(Tensor.matmul, a, b)
     tflops = (2 * (n ** 3) / sec) / 1e12
     return tflops
 
 def tinygrad_benchmark_tflops():
-    f32 = round(time_matmul(n=1024, dtype=dtypes.float32), 2)
-    f16 = round(time_matmul(n=1024, dtype=dtypes.float16), 2)
+    n = 2048
+    f32 = round(_benchmark_matmul(n, dtype=dtypes.float32), 2)
+    f16 = round(_benchmark_matmul(n, dtype=dtypes.float16), 2)
     return (f32, f16, 0.0)
 
 if __name__ == "__main__":
