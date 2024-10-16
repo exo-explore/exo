dcd3264c81b68739ec630c080c7215a0f4ac300f
Eyasu Nigussie <eyasunigussie@Eyasus-MacBook-Air.local>
2024-10-15 21:53:11 -0700

    added ci/cd

--- build/build_exo.py
+++ build/build_exo.py
@@ -1,37 +1,54 @@
 import subprocess
+import sys
 
-def build_macapp():
+def run():
+
     command = [
         "python3", "-m", "nuitka", "exo/main.py",
+        "--company-name=exolabs",
         "--output-dir=dist",
-        "--macos-app-name=exo",
-        "--macos-app-mode=gui",
-        "--macos-app-version=0.0.1",
-        "--product-name=exo",
         "--follow-imports",
         "--standalone",
-        "--macos-create-app-bundle",
-        "--macos-app-icon=/Users/eyasunigussie/exo/docs/exo-logo.icns",
+        "--output-filename=exo"
+    ]
+
+    if sys.platform == "darwin":  
+        command.extend([
+            "--macos-app-name=exo",
+            "--macos-app-mode=gui",
+            "--macos-app-version=0.0.1",
+            "--product-name=exo",
+            "--macos-create-app-bundle",
+            "--macos-app-icon=../docs/exo-logo.icns"
+        ])
+    elif sys.platform == "win32":  
+        command.extend([
+            "--windows-icon-from-ico=../docs/exo-logo.ico"  
+        ])
+    elif sys.platform.startswith("linux"):  
+        command.extend([
+            "--linux-icon=../docs/exo-logo.png"
+        ])
+
+    command.extend([
         "--include-distribution-meta=pygments",
         "--include-distribution-meta=mlx",
         "--include-module=mlx._reprlib_fix",
         "--include-module=mlx._os_warning",
-        "--output-filename=exo",
         "--include-data-files=./.venv/lib/python3.11/site-packages/mlx/lib/mlx.metallib=mlx/lib/mlx.metallib",
         "--include-data-dir=exo/tinychat=tinychat",
-        "--include-package=models",
         "--include-module=exo.inference.mlx.models.llama",
         "--include-module=exo.inference.mlx.models.deepseek_v2",
         "--include-module=exo.inference.mlx.models.base",
         "--include-module=exo.inference.mlx.models.llava",
         "--include-module=exo.inference.mlx.models.qwen2"
-    ]
-    
+    ])
+
     try:
         subprocess.run(command, check=True)
-        print("Build completed successfully.")
+        print("Build completed!")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
-    
+
 if __name__ == "__main__":
-    build_macapp()
+    run()
