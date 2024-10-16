eda1ffdb5495b6ff577ff6f4e9b34a1a76123f04
Eyasu Nigussie <eyasunigussie@Eyasus-MacBook-Air.local>
2024-10-15 21:59:50 -0700

    added nuitka to deps

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ install_requires = [
   "Jinja2==3.1.4",
   "netifaces==0.11.0",
   "numpy==2.0.0",
+  "nuitka==2.4.10",
   "nvidia-ml-py==12.560.30",
   "pillow==10.4.0",
   "prometheus-client==0.20.0",
