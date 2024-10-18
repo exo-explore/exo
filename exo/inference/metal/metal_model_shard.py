f027d8135449a36b6e9562a7cfc0179753f34bc9
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2024-10-18 14:34:29 +0530

    Add libraries
    - added libraries and dependencies

---
+++ exo/inference/metal/metal_model_shard.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+from typing import List, Dict, Tuple, Any, Optional, Union
+import numpy as np
+from tinygrad import dtypes
+from tinygrad.codegen.kernel import Kernel
+from .utils import KernelOperation , Linearizer,Kernel
