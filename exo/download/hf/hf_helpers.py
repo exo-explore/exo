f337e78119ec799b34a1bbaf68ade9c9571d5f05
josh <eyasufikru567@gmail.com>
2024-11-19 10:26:15 -0800

    pr fixes

--- exo/download/hf/hf_helpers.py
+++ exo/download/hf/hf_helpers.py
@@ -110,15 +110,14 @@ async def move_models_to_hf(seed_dir: Union[str, Path]):
   source_dir = Path(seed_dir)
   dest_dir = get_hf_home()/"hub"
   await aios.makedirs(dest_dir, exist_ok=True)
-  for path in source_dir.iterdir():
+  async for path in async_iterdir(source_dir):  
     if path.is_dir() and path.name.startswith("models--"):
-        dest_path = dest_dir / path.name
-        if dest_path.exists():
-            if DEBUG >= 1:
-                print(f"skipping moving {dest_path}. File already exists")
-        else:
-            await aios.rename(str(path), str(dest_path))
-        
+      dest_path = dest_dir / path.name
+      if await async_exists(dest_path): 
+          if DEBUG >= 1: print(f"skipping moving {dest_path}. File already exists")
+      else:
+          await aios.rename(str(path), str(dest_path))
+      
 
 async def fetch_file_list(session, repo_id, revision, path=""):
   api_url = f"{get_hf_endpoint()}/api/models/{repo_id}/tree/{revision}"
