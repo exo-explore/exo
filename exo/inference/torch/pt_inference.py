0320c50252641770857dbd5524191ad937aba079
risingsunomi <vincentcastro@gmail.com>
2024-11-24 05:44:27 -0900

    trying to offload as I can

--- exo/inference/torch/pt_inference.py
+++ exo/inference/torch/pt_inference.py
@@ -110,9 +110,9 @@ class TorchDynamicShardInferenceEngine(InferenceEngine):
       )
 
       if model_hs is not None:
-        return model_hs.numpy(force=True)
+        return model_hs.cpu().numpy(force=True)
 
-      return model_logits.numpy(force=True)
+      return model_logits.cpu().numpy(force=True)
 
     return await asyncio.get_running_loop().run_in_executor(self.executor, infer_wrapper)
 
