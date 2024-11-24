a170cc67152a1287b7c2e295c9654e8bbb71f64b
risingsunomi <vincentcastro@gmail.com>
2024-11-23 23:03:36 -0900

    adding torchtune install

--- setup.py
+++ setup.py
@@ -28,7 +28,8 @@ install_requires = [
   "uuid==1.30",
   "tinygrad @ git+https://github.com/tinygrad/tinygrad.git@232edcfd4f8b388807c64fb1817a7668ce27cbad",
   "torch==2.4.0",
-  "accelerate==0.34.2"
+  "accelerate==0.34.2",
+  "torchtune==0.4.0"
 ]
 
 extras_require = {
