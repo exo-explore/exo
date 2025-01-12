9b2c01c87396623df9c78ed867f5f49025da2719
Alex Cheema <alexcheema123@gmail.com>
2025-01-12 03:15:49 +0000

    ensure dir exists

--- exo/helpers.py
+++ exo/helpers.py
@@ -331,14 +331,14 @@ def is_frozen():
 def get_exo_home() -> Path:
   if psutil.WINDOWS: docs_folder = Path(os.environ["USERPROFILE"]) / "Documents"
   else: docs_folder = Path.home() / "Documents"
-  if not docs_folder.exists(): docs_folder.mkdir()
+  if not docs_folder.exists(): docs_folder.mkdir(exist_ok=True)
   exo_folder = docs_folder / "Exo"
-  if not exo_folder.exists(): exo_folder.mkdir()
+  if not exo_folder.exists(): exo_folder.mkdir(exist_ok=True)
   return exo_folder
 
 def get_exo_images_dir() -> Path:
   exo_home = get_exo_home()
   images_dir = exo_home / "Images"
-  if not images_dir.exists(): images_dir.mkdir()
+  if not images_dir.exists(): images_dir.mkdir(exist_ok=True)
   return images_dir
   
