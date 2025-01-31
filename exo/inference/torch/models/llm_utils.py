a7757d305166d853452355c145db2f99a6f54d35
risingsunomi <vincentcastro@gmail.com>
2025-01-30 21:48:23 -0900

    adding qwen2 model, creating a general multihead attention transformal model to route needed model structure but same base mha structure, updated tests, updated inference engine, generalized some llm functions in llm_utils.py

--- exo/inference/torch/models/llm_utils.py
+++ exo/inference/torch/models/llm_utils.py
@@ -5,11 +5,12 @@ import os
 import re
 import json
 from pathlib import Path
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union, List, Callable
 
 import torch
 import torch.nn as nn
-from torchtune.modules import FeedForward
+from torchtune.modules.attention_utils import _MaskType
+from torchtune.modules import FeedForward, TransformerDecoder
 from torchtune.models.convert_weights import hf_to_tune
 
 from safetensors.torch import load_file as load_safetensors
@@ -147,11 +148,10 @@ def load_model_weights_torchtune(
   if head_dim is None:
     head_dim = dim // num_heads
 
-  
-
   model_state_dict = model.state_dict()
-  for name, _ in model_state_dict.items():
-    print(f"name {name}")
+  if DEBUG >= 8:
+    for name, _ in model_state_dict.items():
+      print(f"name {name}")
   # Load weights from safetensors files in the cache directory
   safetensors_files = list(cache_dir.glob("*.safetensors"))
   if not safetensors_files:
@@ -171,74 +171,91 @@ def load_model_weights_torchtune(
   # remap to work with our model
   remapped_state_dict = {}
 
-  if "llama" in shard.model_id or "Llama" in shard.model_id:
-    for key, value in full_state_dict.items():
-      # load layer by shard
-      for layer_num in range(shard.start_layer, shard.end_layer + 1):
-        # change input layer norm to sa_norm for torchtune
-        re_iln = re.findall(rf"model.layers\.{layer_num}\.(input_layernorm)\.weight", key)
-        if len(re_iln) != 0:
-          new_key = f"model.layers.{layer_num}.sa_norm.scale"
+  is_llama = True if "llama" in shard.model_id or "Llama" in shard.model_id else False
+  
+  if DEBUG >= 8 and is_llama:
+    print("loading llama type weights")
+  elif DEBUG >= 8 and not is_llama:
+    print("loading weights")
+
+  for key, value in full_state_dict.items():
+    # load layer by shard
+    for layer_num in range(shard.start_layer, shard.end_layer + 1):
+      # change input layer norm to sa_norm for torchtune
+      re_iln = re.findall(rf"model.layers\.{layer_num}\.(input_layernorm)\.weight", key)
+      if len(re_iln) != 0:
+        new_key = f"model.layers.{layer_num}.sa_norm.scale"
+        remapped_state_dict[new_key] = value
+        if DEBUG >= 8:
+          print(f"{key} == {new_key}")
+
+      # change post attention layernorm to mlp_norm for torchtune
+      re_pal = re.findall(rf"model.layers\.{layer_num}\.(post_attention_layernorm)\.weight", key)
+      if len(re_pal) != 0:
+        new_key = f"model.layers.{layer_num}.mlp_norm.scale"
+        remapped_state_dict[new_key] = value
+        if DEBUG >= 8:
+          print(f"{key} == {new_key}")
+
+      # change self_attn to attn
+      # along with changing o_proj to output_proj
+      re_attn = re.findall(rf"model\.layers\.{layer_num}.(\w+)\.(\w+)\.(\w+)", key)
+      if len(re_attn) != 0 and re_attn[0][0] == "self_attn":
+        if re_attn[0][1] == "k_proj" and is_llama:
+          value = _permute(
+            t=value,
+            n_heads=num_kv_heads,
+            head_dim=head_dim,
+            dim=dim
+          )
+
+          new_key = f"model.layers.{layer_num}.attn.{re_attn[0][1]}.{re_attn[0][2]}"
           remapped_state_dict[new_key] = value
-          if DEBUG >= 8:
-            print(f"{key} == {new_key}")
-
-        # change post attention layernorm to mlp_norm for torchtune
-        re_pal = re.findall(rf"model.layers\.{layer_num}\.(post_attention_layernorm)\.weight", key)
-        if len(re_pal) != 0:
-          new_key = f"model.layers.{layer_num}.mlp_norm.scale"
+        elif re_attn[0][1] == "q_proj" and is_llama:
+          value = _permute(
+            t=value,
+            n_heads=num_heads,
+            head_dim=head_dim,
+            dim=dim
+          )
+          new_key = f"model.layers.{layer_num}.attn.{re_attn[0][1]}.{re_attn[0][2]}"
           remapped_state_dict[new_key] = value
-          if DEBUG >= 8:
-            print(f"{key} == {new_key}")
 
-        # change self_attn to attn
-        # along with changing o_proj to output_proj
-        re_attn = re.findall(rf"model\.layers\.{layer_num}.(\w+)\.(\w+)\.(\w+)", key)
-        if len(re_attn) != 0 and re_attn[0][0] == "self_attn":
-          if re_attn[0][1] == "k_proj":
-            value = _permute(
-              t=value,
-              n_heads=num_kv_heads,
-              head_dim=head_dim,
-              dim=dim
-            )
-
-            new_key = f"model.layers.{layer_num}.attn.{re_attn[0][1]}.{re_attn[0][2]}"
-            remapped_state_dict[new_key] = value
-          elif re_attn[0][1] == "q_proj":
-            value = _permute(
-              t=value,
-              n_heads=num_heads,
-              head_dim=head_dim,
-              dim=dim
-            )
-            new_key = f"model.layers.{layer_num}.attn.{re_attn[0][1]}.{re_attn[0][2]}"
-            remapped_state_dict[new_key] = value
-
-          elif re_attn[0][1] == "o_proj":
-            new_key = f"model.layers.{layer_num}.attn.output_proj.weight"
-            remapped_state_dict[new_key] = value
-          else:
-            new_key = f"model.layers.{layer_num}.attn.{re_attn[0][1]}.{re_attn[0][2]}"
-            remapped_state_dict[new_key] = value
-          if DEBUG >= 8:
-            print(f"{key} == {new_key}")
-
-        # set mlp weights
-        re_mlp = re.findall(rf"model\.layers\.{layer_num}.mlp.(\w+)\.(\w+)", key)
-        if len(re_mlp) != 0:
-          proj_name = re_mlp[0][0]
-          if proj_name == "up_proj":
-            proj_name = "w3"
-          elif proj_name == "down_proj":
-            proj_name = "w2"
-          elif proj_name == "gate_proj":
-            proj_name = "w1"
-          new_key = f"model.layers.{layer_num}.mlp.{proj_name}.weight"
+        elif re_attn[0][1] == "o_proj":
+          new_key = f"model.layers.{layer_num}.attn.output_proj.weight"
           remapped_state_dict[new_key] = value
-          if DEBUG >= 8:
-            print(f"{key} == {new_key}")
+        else:
+          new_key = f"model.layers.{layer_num}.attn.{re_attn[0][1]}.{re_attn[0][2]}"
+          remapped_state_dict[new_key] = value
+        if DEBUG >= 8:
+          print(f"{key} == {new_key}")
 
+      # set mlp weights
+      re_mlp = re.findall(rf"model\.layers\.{layer_num}.mlp.(\w+)\.(\w+)", key)
+      if len(re_mlp) != 0:
+        proj_name = re_mlp[0][0]
+        if proj_name == "up_proj":
+          proj_name = "w3"
+        elif proj_name == "down_proj":
+          proj_name = "w2"
+        elif proj_name == "gate_proj":
+          proj_name = "w1"
+        new_key = f"model.layers.{layer_num}.mlp.{proj_name}.weight"
+        remapped_state_dict[new_key] = value
+        if DEBUG >= 8:
+          print(f"{key} == {new_key}")
+
+    # saving embed for paired weights
+    if key == "model.embed_tokens.weight":
+      remapped_state_dict["model.tok_embeddings.weight"] = value
+      if DEBUG >= 8:
+        print("model.embed_tokens.weight == model.tok_embeddings.weight")
+
+    if key == "model.norm.weight":
+      remapped_state_dict["model.norm.scale"] = value
+
+    if key == "lm_head.weight":
+      remapped_state_dict["model.output.weight"] = value
       # saving embed for paired weights
       if key == "model.embed_tokens.weight":
         remapped_state_dict["model.tok_embeddings.weight"] = value
@@ -251,9 +268,6 @@ def load_model_weights_torchtune(
       if key == "lm_head.weight":
         remapped_state_dict["model.output.weight"] = value
 
-  else:
-    print(f"{shard.model_id} not supported for sharding, loading weights normally")
-
   if not remapped_state_dict:
     model.load_state_dict(full_state_dict, strict=True)
   else:
@@ -265,10 +279,164 @@ def load_model_weights_torchtune(
     # load new weight map
     model.load_state_dict(remapped_state_dict, strict=False)
 
-    # if DEBUG >= 8:
-    print("\n--- checking weights ----\n")
-    check_weights(model, remapped_state_dict)
+    if DEBUG >= 8:
+      print("\n--- checking weights ----\n")
+      check_weights(model, remapped_state_dict)
 
+class ShardTransformerDecoder(TransformerDecoder):
+  """
+  ShardTransformerDecorder
+  Custom version of torchtune TransformerDecoder to allow for
+  sharding of models and passing of hidden layers between shards
+  """
+  def __init__(
+    self,
+    *,
+    shard: Shard,
+    tok_embeddings: nn.Embedding,
+    layers: Union[nn.Module, List[nn.Module], nn.ModuleList],
+    max_seq_len: int,
+    num_heads: int,
+    head_dim: int,
+    norm: nn.Module,
+    output: Union[nn.Linear, Callable],
+    num_layers: Optional[int] = None,
+    output_hidden_states: Optional[List[int]] = None,
+  ):
+    super().__init__(
+      tok_embeddings=tok_embeddings,
+      layers=layers,
+      max_seq_len=max_seq_len,
+      num_heads=num_heads,
+      head_dim=head_dim,
+      norm=norm,
+      output=output,
+      num_layers=num_layers,
+      output_hidden_states=output_hidden_states,
+    )
+
+    self.shard = shard
+
+  def setup_caches(
+    self,
+    batch_size: int,
+    dtype: torch.dtype,
+    *,
+    encoder_max_seq_len: Optional[int] = None,
+    decoder_max_seq_len: Optional[int] = None,
+  ):
+    """
+    modified version for shard
+
+    assume just decoder layers
+    """
+    if decoder_max_seq_len is not None:
+      self.decoder_max_cache_seq_len = decoder_max_seq_len
+    else:
+      self.decoder_max_cache_seq_len = self.max_seq_len
+
+    for layer in self.layers:
+      if layer is not None:
+        layer.setup_caches(
+          batch_size,
+          dtype,
+          encoder_max_seq_len=self.encoder_max_cache_seq_len,
+          decoder_max_seq_len=self.decoder_max_cache_seq_len,
+        )
+
+  def caches_are_enabled(self) -> bool:
+    """
+    modified version for shard
+    """
+    if self.layers[0] is not None:
+      return self.layers[0].caches_are_enabled()
+    else:
+      for layer in self.layers:
+        if layer is not None:
+          return layer.caches_are_enabled()
+
+    return False
+
+  def reset_caches(self):
+    torch.cuda.empty_cache()
+
+    for layer in self.layers:
+      if layer is not None:
+        layer.reset_cache()
+
+  def forward(
+    self,
+    tokens: torch.Tensor,
+    *,
+    mask: Optional[_MaskType] = None,
+    input_pos: Optional[torch.Tensor] = None,
+    hidden_state: Optional[torch.Tensor] = None,
+    dtype: torch.dtype = torch.float16
+  ) -> Union[torch.Tensor, List[torch.Tensor]]:
+    # Determine the type of input and shape
+    if DEBUG >= 4:
+      print("forward called")
+      if tokens is not None:
+        print(f"tokens [{tokens.shape}]: {tokens}")
+        print(f"mask: {mask}")
+        print(f"input_pos: {input_pos}")
+
+      if hidden_state is not None:
+        print(f"hidden_state [{hidden_state.shape}]: {hidden_state}")
+
+    if hidden_state is not None:
+      h = hidden_state  # Use directly as hidden states
+    else:
+      seq_len = tokens.shape[1]
+
+      self._validate_inputs(
+        seq_len,
+        mask=mask,
+        input_pos=input_pos,
+      )
+
+      fl_tokens = tokens.clone()
+      h = self.tok_embeddings(fl_tokens).to(dtype=dtype)  # Apply token tok_embeddings
+
+    # Initialize a list to capture hidden states if requested
+    # for captured hidden states
+    hidden = []
+    curr_layers = [self.layers[i] for i in range(self.shard.start_layer, self.shard.end_layer + 1)]
+    for i, layer in enumerate(curr_layers):
+      if DEBUG >= 8:
+        print(f"\nhidden layer in H[{self.shard.start_layer+i}]\n{h}")
+        print(f"\nmask\n{mask}\ninput_pos\n{input_pos}")
+        print(f"\noutput_hidden_states\n{self.output_hidden_states}\n")
+
+      if i in self.output_hidden_states:
+        hidden.append(h)
+
+      # Process through each transformer layer
+      h = layer(
+        h,
+        mask=mask,
+        input_pos=input_pos,
+      )
+
+      if DEBUG >= 8:
+        print(f"\nhidden layer out H[{self.shard.start_layer+i}]->H[{self.shard.start_layer+i+1}]\n{h}\n")
+
+    if self.shard.is_last_layer():
+      # Apply normalization
+      h = self.norm(h)
+
+      # Handle chunked output if needed
+      output = self.output(h).float()
+
+      if DEBUG >= 4:
+        print(f"\n\noutput {output}\n\n")
+
+      return output
+    else:
+      if DEBUG >= 4:
+        print(f"\n\nhidden output {hidden[-1]}\n\n")
+
+      return hidden[-1]
 
 class MultiLayerPreceptron(nn.Module):
   def __init__(self, input_dim, hidden_dim, activation="silu", use_bias=False):
@@ -299,36 +467,6 @@ class MultiLayerPreceptron(nn.Module):
 
   def forward(self, x) -> torch.Tensor:
     return self.down_proj(self.act_fn(self.gate_proj(x))*self.up_proj(x))
-
-
-class RMSNorm(nn.Module):
-  def __init__(self, hidden_size, eps=1e-6):
-    """
-    RMSNorm 
-    designed for llama model but used for other models
-    """
-    super().__init__()
-    self.weight = nn.Parameter(torch.ones(hidden_size))
-    self.eps = eps
-
-  def forward(self, hidden_states):
-    input_dtype = hidden_states.dtype
-    hidden_states = hidden_states.to(torch.float32)
-    variance = hidden_states.pow(2).mean(-1, keepdim=True)
-    hidden_states = hidden_states*torch.rsqrt(variance + self.eps)
-    return self.weight*hidden_states.to(input_dtype)
-
-
-def llama3_mlp(dim: int, hidden_dim: int) -> FeedForward:
-  """
-  Build the MLP layer associated with the Llama model.
-  """
-  gate_proj = nn.Linear(dim, hidden_dim, bias=False)
-  down_proj = nn.Linear(hidden_dim, dim, bias=False)
-  up_proj = nn.Linear(dim, hidden_dim, bias=False)
-
-  return FeedForward(gate_proj=gate_proj, down_proj=down_proj, up_proj=up_proj)
-
 
 class ShardInferenceState:
   def __init__(
@@ -369,3 +507,41 @@ class ShardInferenceState:
     mask: {self.mask}
     curr_pos: {self.curr_pos}
     """
+
+def layer_mlp(dim: int, hidden_dim: int) -> FeedForward:
+  """
+  Generalized MLP layer
+  Ref: https://github.com/pytorch/torchtune/blob/main/torchtune/models/llama3_1/_component_builders.py#L124
+  Ref: https://github.com/pytorch/torchtune/blob/main/torchtune/models/qwen2/_component_builders.py#L127C1-L134C82
+  """
+  gate_proj = nn.Linear(dim, hidden_dim, bias=False)
+  down_proj = nn.Linear(hidden_dim, dim, bias=False)
+  up_proj = nn.Linear(dim, hidden_dim, bias=False)
+  return FeedForward(gate_proj=gate_proj, down_proj=down_proj, up_proj=up_proj)
+
+"""
+Llama utils
+"""
+def llama3_mlp(dim: int, hidden_dim: int) -> FeedForward:
+  """
+  Build the MLP layer associated with the Llama model.
+  Ref: https://github.com/pytorch/torchtune/blob/main/torchtune/models/llama3_1/_component_builders.py#L124
+  """
+  gate_proj = nn.Linear(dim, hidden_dim, bias=False)
+  down_proj = nn.Linear(hidden_dim, dim, bias=False)
+  up_proj = nn.Linear(dim, hidden_dim, bias=False)
+
+  return FeedForward(gate_proj=gate_proj, down_proj=down_proj, up_proj=up_proj)
+
+"""
+Qwen utils
+"""
+def qwen2_mlp(dim: int, hidden_dim: int) -> FeedForward:
+  """
+  Build the MLP layer associated with the Qwen2 model.
+  Ref: https://github.com/pytorch/torchtune/blob/main/torchtune/models/qwen2/_component_builders.py#L127C1-L134C82
+  """
+  gate_proj = nn.Linear(dim, hidden_dim, bias=False)
+  down_proj = nn.Linear(hidden_dim, dim, bias=False)
+  up_proj = nn.Linear(dim, hidden_dim, bias=False)
+  return FeedForward(gate_proj=gate_proj, down_proj=down_proj, up_proj=up_proj)
