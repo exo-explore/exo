0560640ace78f37b6facb8946414186e3e63ae77
Joshua Coles <me@joshuacoles.me>
2025-02-19 15:39:08 +0000

    Strip EOS token from output to mirror the OpenAI behaviour

--- exo/api/chatgpt_api.py
+++ exo/api/chatgpt_api.py
@@ -387,7 +387,14 @@ class ChatGPTAPI:
             if not eos_token_id and hasattr(tokenizer, "_tokenizer"): eos_token_id = tokenizer.special_tokens_map.get("eos_token_id")
 
             finish_reason = None
-            if is_finished: finish_reason = "stop" if tokens[-1] == eos_token_id else "length"
+            if is_finished:
+              if tokens[-1] == eos_token_id:
+                # We do not return the EOS token in the response
+                tokens.pop(-1)
+                finish_reason = "stop"
+              else:
+                finish_reason = "length"
+
             if DEBUG >= 2: print(f"{eos_token_id=} {tokens[-1]=} {finish_reason=}")
 
             completion = generate_completion(
@@ -440,6 +447,8 @@ class ChatGPTAPI:
         if not eos_token_id and hasattr(tokenizer, "_tokenizer"): eos_token_id = tokenizer.special_tokens_map.get("eos_token_id")
         if DEBUG >= 2: print(f"Checking if end of tokens result {tokens[-1]=} is {eos_token_id=}")
         if tokens[-1] == eos_token_id:
+          # We do not return the EOS token in the response
+          tokens.pop(-1)
           finish_reason = "stop"
 
         return web.json_response(generate_completion(chat_request, tokenizer, prompt, request_id, tokens, stream, finish_reason, "chat.completion"))
