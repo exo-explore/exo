fca30828df94008621ddde3296f57a8a90c3ab6c
Dead-Bytes <divyraj.saini22b@iiitg.ac.in>
2025-03-06 23:46:58 +0530

    "build ensure  hard"

--- exo/inference/huggingface/inference.py
+++ exo/inference/huggingface/inference.py
@@ -1,8 +1,14 @@
 from exo.inference.inference_engine import InferenceEngine
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from exo.inference.shard import Shard
+from exo.download.shard_download import ShardDownloader
 
+def download_model_from_model_id(model_id):
+    pass
 
 class HuggingfaceInferenceEngine(InferenceEngine):
     def __init__(self):
+        self.shard = None
         pass
     
     async def encode(self, shard, prompt):
@@ -20,8 +26,17 @@ class HuggingfaceInferenceEngine(InferenceEngine):
     async def load_checkpoint(self, shard, path):
         pass
     
-    async def ensure_shard(self, shard):
-        pass
+    async def ensure_shard(self, shard: Shard):
+        """make hure the ard exists otherwise downloads the model and the tokenizer and initialise the shard """
+        if self.shard == shard:
+            return
+        
+        model_path = await self.shard_downloader.ensure_shard(shard, self.__class__.__name__)
+        if self.shard != shard:
+            self.shard = shard
+            self.model = AutoModelForCausalLM.from_pretrained(model_path)
+            self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+    
     
     async def infer_prompt(self, request_id, shard, prompt, inference_state=None):
         pass
