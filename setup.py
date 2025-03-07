514c03e9dd4464e4d778fcf0ff97a66bf7d5b3aa
yuzhoulu <yuzhoulu@amd.com>
2025-03-07 15:35:38 +0800

    1. change requirements and settings to find tunygrad 2. add test scripts on tinygrad_nvwq

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requires = [
   "transformers==4.46.3",
   "uuid==1.30",
   "uvloop==0.21.0",
-  "tinygrad @ git+https://github.com/tinygrad/tinygrad.git@ec120ce6b9ce8e4ff4b5692566a683ef240e8bc8",
+  "tinygrad==0.10.2",
   "torch==2.6.0",
   "accelerate==0.34.2",
   "torchtune==0.5.0",
