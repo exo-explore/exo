b62f2aa03eedd23bf3dad0a2fcc9445283d8067c
Dead-Bytes <divyraj.saini22b@iiitg.ac.in>
2025-03-07 09:07:03 +0530

    "sample built"

--- exo/inference/huggingface/inference.py
+++ exo/inference/huggingface/inference.py
@@ -2,20 +2,37 @@ from exo.inference.inference_engine import InferenceEngine
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from exo.inference.shard import Shard
 from exo.download.shard_download import ShardDownloader
+from transformers import LogitsProcessor, LogitsProcessorList, TemperatureLogitsWarper, TopPLogitsWarper
+import numpy as np
+import torch
+import asyncio
 
+TEMPERATURE = 0.85
 def download_model_from_model_id(model_id):
     pass
 
 class HuggingfaceInferenceEngine(InferenceEngine):
     def __init__(self, shard_downloader: ShardDownloader):
+        self.shard = None
         self.shard_downloader = shard_downloader
+        
         pass
     
     async def encode(self, shard, prompt):
         pass
     
-    async def sample(self, x):
-        pass
+    async def sample(self, x: np.ndarray, temp=TEMPERATURE, top_p: float = 0.0) -> np.ndarray:
+        def sample_wrapper():
+            logits = torch.Tensor(x[:, -1, :])
+            processors = LogitsProcessorList([
+            TemperatureLogitsWarper(temp),
+            TopPLogitsWarper(top_p) if top_p > 0 else None
+            ])
+            filtered_logits = processors(None, logits.numpy())
+            probs = torch.nn.softmax(torch.Tensor(filtered_logits))
+            return probs.multinomial().realize().numpy().astype(int)
+        
+        return await asyncio.get_running_loop().run_in_executor(self.executor, sample_wrapper)
     
     async def decode(self, shard, tokens):
         pass
