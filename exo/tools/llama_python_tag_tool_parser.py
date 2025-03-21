5dc94b120c78a9a9297160b695bdd6644cf7ab3f
Joshua Coles <me@joshuacoles.me>
2025-03-21 06:46:02 +0000

    Add support for parallel llama python tag calling

--- exo/tools/llama_python_tag_tool_parser.py
+++ exo/tools/llama_python_tag_tool_parser.py
@@ -14,34 +14,52 @@ class LlamaPythonTag(ToolParser):
     return chunk.tokens[0] == 128010
 
   def to_grammar(self, tools: list[ToolDefinition], required: bool, parallel_tool_calling: bool) -> str:
-    if parallel_tool_calling:
-      raise ValueError("Parallel tool calling not supported for LlamaPythonTag")
+    tool_call_schema = generate_tool_call_json_schema(tools, "parameters")
+
+    # This implements a parallel format of <|python_tag|> json array of tool calls <|eom_id|>.
+    # I think this is the best option as the other obvious alternative is <|python_tag|> tool call 1 <|eom_id|><|python_tag|> tool call 2 <|eom_id|>.
+    # However, the model has been observed to repeat the same tool call multiple times in a row when not stopped at <|eom_id|> so this is not ideal.
+    parallel_tool_call_schema = {
+      "type": "array",
+      "items": tool_call_schema
+    }
+
+    function_call_production = "parallel_fun_call" if parallel_tool_calling else "single_fun_call"
+    entry_production = function_call_production if required else "TEXT | single_fun_call"
 
     # This is lifted from https://github.com/guidance-ai/llguidance/blob/cc83715f/docs/syntax.md#special-tokens
     return lark_grammar(f"""
 %llguidance {{}}
 
-start: {"fun_call" if required else "TEXT | fun_call"}
+start: {entry_production}
 TEXT: /[^{{](.|\n)*/
-fun_call: <|python_tag|> json_body <|eom_id|>
-json_body: %json{json.dumps(generate_tool_call_json_schema(tools, "parameters"))}
+single_fun_call: <|python_tag|> json_body <|eom_id|>
+parallel_fun_call: <|python_tag|> parallel_json_body <|eom_id|>
+json_body: %json{json.dumps(tool_call_schema)}
+parallel_json_body: %json{json.dumps(parallel_tool_call_schema)}
     """.strip())
 
   def parse_complete(self, content: str, parallel_tool_calling: bool = False) -> list[UnplacedToolCall]:
-    if parallel_tool_calling:
-      raise ValueError("Parallel tool calling not supported for LlamaPythonTag")
-
     tool_calls = []
 
     for m in re.finditer(r"<\|python_tag\|>(.+)<\|eom_id\|>", content, re.DOTALL):
       try:
-        remapped = json.loads(m.group(1))
+        if parallel_tool_calling:
+          array = json.loads(m.group(1))
 
-        # Rename "parameters" to "arguments" as that is the expected format
-        tool_calls.append(UnplacedToolCall(
-          name=remapped["name"],
-          arguments=json.dumps(remapped["parameters"])
-        ))
+          for raw_tool_call in array:
+            tool_calls.append(UnplacedToolCall(
+              name=raw_tool_call["name"],
+              arguments=json.dumps(raw_tool_call["parameters"])
+            ))
+        else:
+          raw_tool_call = json.loads(m.group(1))
+
+          # Rename "parameters" to "arguments" as that is the expected format
+          tool_calls.append(UnplacedToolCall(
+            name=raw_tool_call["name"],
+            arguments=json.dumps(raw_tool_call["parameters"])
+          ))
       except json.JSONDecodeError as e:
         if DEBUG >= 2: print(f"Failed to parse python_tag tool calls: {e}")
 
