ae734b8175fcae2c655c80a820af186a93de1b6a
Dawid Wiśniewski <dw949@wp.pl>
2025-04-01 17:39:57 +0000

    fix

--- exo/inference/tinygrad/load_by_layer.py
+++ exo/inference/tinygrad/load_by_layer.py
@@ -94,4 +94,67 @@ def build_transformer_2(model_path: str, shard: Shard = None, model_size="8B", v
             load_state_dict(model, weights, strict=False,
                             consume=True, verbose=verbose)
 
-        if shard.end_layer == MODEL_PARAMS[model_size]['args']['
+        if shard.end_layer == MODEL_PARAMS[model_size]['args']['n_layers'] - 1:
+            weights = safe_load_by_layer(model_path, l="model.norm")
+            weights = convert_from_huggingface3(
+                weights, model, MODEL_PARAMS[model_size]['args']['n_heads'], MODEL_PARAMS[model_size]['args']['n_kv_heads'])
+            weights = fix_bf16(weights)
+
+            load_state_dict(model, weights, strict=False,
+                        consume=True, verbose=verbose) 
+            
+            weights = safe_load_by_layer(model_path, l="output.weight")
+            weights = convert_from_huggingface3(
+                weights, model, MODEL_PARAMS[model_size]['args']['n_heads'], MODEL_PARAMS[model_size]['args']['n_kv_heads'])
+            weights = fix_bf16(weights)
+
+            load_state_dict(model, weights, strict=False,
+                        consume=True, verbose=verbose)
+        model = TransformerShard(shard, model)
+    return model
+
+MODEL_PARAMS = {
+  "1B": {
+    "args": {
+      "dim": 2048, "n_heads": 32, "n_kv_heads": 8, "n_layers": 16, "norm_eps": 1e-5, "rope_theta": 500000, "vocab_size": 128256, "hidden_dim": 8192,
+      "rope_scaling": {"factor": 32.0, "high_freq_factor": 4.0, "low_freq_factor": 1.0, "original_max_position_embeddings": 8192, "rope_type": "llama3"}, "tie_word_embeddings": True
+    }, "files": 1
+  }, "3B": {
+    "args": {
+      "dim": 3072, "n_heads": 24, "n_kv_heads": 8, "n_layers": 28, "norm_eps": 1e-5, "rope_theta": 500000, "vocab_size": 128256, "hidden_dim": 8192,
+      "rope_scaling": {"factor": 32.0, "high_freq_factor": 4.0, "low_freq_factor": 1.0, "original_max_position_embeddings": 8192, "rope_type": "llama3"}, "tie_word_embeddings": True
+    }, "files": 1
+  }, "8B": {"args": {"dim": 4096, "n_heads": 32, "n_kv_heads": 8, "n_layers": 32, "norm_eps": 1e-5, "rope_theta": 500000, "vocab_size": 128256, "hidden_dim": 14336}, "files": 1},
+  "70B": {
+      "args": {""
+        "dim": 8192, "n_heads": 64, "n_kv_heads": 8, "n_layers": 80, "norm_eps": 1e-5, "rope_theta": 500000, "vocab_size": 128256, "hidden_dim": 28672
+      }, 
+      "files": 8
+    },
+  "32B": {
+    "args": {
+          "dim": 5120, "n_heads": 40, "n_kv_heads": 8, "n_layers": 64, "norm_eps": 1e-5, "rope_theta": 1000000, "vocab_size": 152064, "hidden_dim": 27648
+    },
+    "files": 17,
+    "tokenizer": "Qwen/QwQ-32B-Preview",
+  }
+}
+
+import time
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Playground for tinygrad inference.")
+    parser.add_argument("--model", type=str, default="1B", help="Model size")
+    parser.add_argument("--model-path", type=str,
+                        default="models/1B", help="Path to model")
+    parser.add_argument("--shard", type=str, default="0", help="Shard index")
+    args = parser.parse_args()
+
+    model2 = build_transformer_2(Path(args.model_path), shard=Shard(
+        '1', 0, 15, 16), model_size=args.model)
+    model = build_transformer(Path(args.model_path), shard=Shard(
+        args.shard, 0, 15, 16), model_size=args.model)
+
+    w_model = nn.state.get_state_dict(model)['norm.weight'][:3].numpy()
+    w_model2 = nn.state.get_state_dict(model2)['norm.weight'][:3].numpy()
+    assert (w_model == w_model2).all(), "Weights are not equal"
