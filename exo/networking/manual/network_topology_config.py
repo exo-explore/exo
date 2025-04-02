7aa9f5571de0145de92bfa545db7dc9a78eb7401
Kevin Day <kevin@your.org>
2025-04-02 16:35:22 -0500

    Document the format of the manual configuration file.
    Add --get-device-capabilities that outputs the current device's
    device_capabilities object in JSON form, to allow external tooling
    to automate the creation of the manual config file.

--- exo/networking/manual/network_topology_config.py
+++ exo/networking/manual/network_topology_config.py
@@ -16,6 +16,44 @@ class NetworkTopology(BaseModel):
   peers: Dict[str, PeerConfig]
   """
   node_id to PeerConfig. The node_id is used to identify the peer in the discovery process. The node that this is running from should be included in this dict.
+  
+  Example configuration file format:
+  ```json
+  {
+    "peers": {
+      "node1": {
+        "address": "192.168.1.10",
+        "port": 50051,
+        "device_capabilities": {
+          "model": "Mac Studio",
+          "chip": "Apple M3 Ultra",
+          "memory": 524288,
+          "flops": {
+            "fp32": 108.52,
+            "fp16": 54.26,
+            "int8": 217.04
+          }
+        }
+      },
+      "node2": {
+        "address": "192.168.1.20",
+        "port": 50051,
+        "device_capabilities": {
+          "model": "Desktop PC",
+          "chip": "NVIDIA GEFORCE GTX 1080 TI",
+          "memory": 11264,
+          "flops": {
+            "fp32": 11.34,
+            "fp16": 0.177,
+            "int8": 45.36
+          }
+        }
+      }
+    }
+  }
+  ```
+  
+  When running with manual discovery, make sure your node_id matches one of the keys in this peers dictionary.
   """
   @classmethod
   def from_path(cls, path: str) -> "NetworkTopology":
