462a891593def1151f89777481cf0bc0cfdab374
Daniel Smith <31456567+smithcoin@users.noreply.github.com>
2025-04-28 17:46:53 -0500

    - update install.sh: handle when python is not installed (#15)
    Co-authored-by: DeftDawg <deftdawg@gmail.com>

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-if command -v python3.12 &>/dev/null; then
-    echo "Python 3.12 is installed, proceeding with python3.12..."
-    python3.12 -m venv .venv
-else
-    echo "The recommended version of Python to run exo with is Python 3.12, but $(python3 --version) is installed. Proceeding with $(python3 --version)"
-    python3 -m venv .venv
-fi
+RECOMMENDED_PYTHON_VER="3.12"
+PYTHON=$(basename $(command -v python${RECOMMENDED_PYTHON_VER} python3 | head -1) 2>/dev/null)
+
+case "${PYTHON}" in
+    python${RECOMMENDED_PYTHON_VER})
+        echo "Python ${RECOMMENDED_PYTHON_VER} is installed, proceeding with python${RECOMMENDED_PYTHON_VER}..."
+        ;;
+    python3)
+        echo "recommended version of Python to run exo with is Python ${RECOMMENDED_PYTHON_VER}, but $(python3 --version) is installed. Proceeding with $(python3 --version)"
+        ;;
+    *)
+        echo "Python ${RECOMMENDED_PYTHON_VER} is not installed, please install Python ${RECOMMENDED_PYTHON_VER}"
+        exit 1
+        ;;
+esac
+
+${PYTHON} -m venv .venv
 source .venv/bin/activate
 pip install -e .
