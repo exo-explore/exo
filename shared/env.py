da50da2b43684806d20f525b56c7befc2bcc8681
Arbion Halili <99731180+ToxicPine@users.noreply.github.com>
2025-06-27 11:57:03 +0100

    Add Simple env.py

--- shared/env.py
+++ shared/env.py
@@ -10,10 +10,6 @@ EnvSchema = TypeVar("EnvSchema", bound=BaseModel)
 def get_validated_env(
     environment_schema: type[EnvSchema], logger: logging.Logger
 ) -> EnvSchema:
-    """
-    Validate and parse data into an instance of config_cls.
-    Raises ValidationError if validation fails.
-    """
     try:
         return environment_schema.model_validate(os.environ, strict=True)
     except ValidationError as e:
