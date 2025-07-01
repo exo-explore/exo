df824e2e87662759bf24bd0a947806ec27d9610c
Arbion Halili <99731180+ToxicPine@users.noreply.github.com>
2025-07-01 12:18:48 +0100

    fix: Ensure MasterState inherits from SharedState

--- shared/types/states/master.py
+++ shared/types/states/master.py
@@ -5,12 +5,13 @@ from pydantic import BaseModel
 from shared.types.common import NodeId
 from shared.types.networking.topology import NetworkState
 from shared.types.profiling.common import NodeProfile
+from shared.types.states.shared import SharedState
 
 
 class ExternalCommand(BaseModel): ...
 
 
-class MasterState(BaseModel):
+class MasterState(SharedState):
     network_state: NetworkState
     node_profiles: dict[NodeId, NodeProfile]
     job_inbox: Queue[ExternalCommand]
