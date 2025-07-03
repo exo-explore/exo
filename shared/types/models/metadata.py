f8039e20e0e9e5f626d2224d1f474d5287b36b96
Arbion Halili <99731180+ToxicPine@users.noreply.github.com>
2025-07-03 12:32:32 +0100

    feature: Add pretty_name to ModelMetadata

--- shared/types/models/metadata.py
+++ shared/types/models/metadata.py
@@ -4,5 +4,6 @@ from pydantic import BaseModel, PositiveInt
 
 
 class ModelMetadata(BaseModel):
+    pretty_name: str
     storage_size_kilobytes: Annotated[int, PositiveInt]
     n_layers: Annotated[int, PositiveInt]
