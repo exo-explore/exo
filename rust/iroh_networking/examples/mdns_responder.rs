30885e30eda53dd039d5ed4b82a98e6de49cb065
Evan <evanev7@gmail.com>
2025-12-01 16:36:51 +0000

    print secret key

--- rust/iroh_networking/examples/mdns_responder.rs
+++ rust/iroh_networking/examples/mdns_responder.rs
@@ -1,4 +1,4 @@
-use iroh::SecretKey;
+use iroh::{SecretKey, endpoint_info::EndpointIdExt};
 use iroh_networking::ExoNet;
 use n0_future::StreamExt;
 
@@ -9,9 +9,10 @@ async fn main() {
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
 
-    let net = ExoNet::init_iroh(SecretKey::generate(&mut rand::rng()), "")
-        .await
-        .unwrap();
+    let key = SecretKey::generate(&mut rand::rng());
+    let dbg_key = key.public().to_z32();
+    println!("Starting with pk: {dbg_key}");
+    let net = ExoNet::init_iroh(key, "").await.unwrap();
 
     let mut conn_info = net.connection_info().await;
 
