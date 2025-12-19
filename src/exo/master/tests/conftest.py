c516ecdb757ef2a80b28aef848387dad3f1a8f19
Evan <evanev7@gmail.com>
2025-12-19 19:21:48 +0000

    fix topology tests

--- src/exo/master/tests/conftest.py
+++ src/exo/master/tests/conftest.py
@@ -1,29 +1,22 @@
-from typing import Callable
-
-import pytest
-
 from exo.shared.types.common import NodeId
 from exo.shared.types.multiaddr import Multiaddr
 from exo.shared.types.profiling import (
-    MemoryPerformanceProfile,
+    MemoryUsage,
     NodePerformanceProfile,
     SystemPerformanceProfile,
 )
-from exo.shared.types.topology import Connection, ConnectionProfile, NodeInfo
+from exo.shared.types.topology import SocketConnection
 
 
-@pytest.fixture
-def create_node():
-    def _create_node(memory: int, node_id: NodeId | None = None) -> NodeInfo:
+def create_node(memory: int, node_id: NodeId | None = None) -> tuple[NodeId, NodePerformanceProfile]:
         if node_id is None:
             node_id = NodeId()
-        return NodeInfo(
-            node_id=node_id,
-            node_profile=NodePerformanceProfile(
+        return (node_id,
+            NodePerformanceProfile(
                 model_id="test",
                 chip_id="test",
                 friendly_name="test",
-                memory=MemoryPerformanceProfile.from_bytes(
+                memory=MemoryUsage.from_bytes(
                     ram_total=1000,
                     ram_available=memory,
                     swap_total=1000,
@@ -34,34 +27,12 @@ def create_node():
             ),
         )
 
-    return _create_node
-
 
 # TODO: this is a hack to get the port for the send_back_multiaddr
-@pytest.fixture
-def create_connection() -> Callable[[NodeId, NodeId, int | None], Connection]:
-    port_counter = 1235
-    ip_counter = 1
+def create_connection(sink_port: int, ip: int) -> SocketConnection:
+    return SocketConnection(
+        sink_multiaddr=Multiaddr(
+            address=f"/ip4/169.254.0.{ip}/tcp/{sink_port}"
+        ),
+    )
 
-    def _create_connection(
-        source_node_id: NodeId, sink_node_id: NodeId, send_back_port: int | None = None
-    ) -> Connection:
-        nonlocal port_counter
-        nonlocal ip_counter
-        # assign unique ips
-        ip_counter += 1
-        if send_back_port is None:
-            send_back_port = port_counter
-            port_counter += 1
-        return Connection(
-            local_node_id=source_node_id,
-            send_back_node_id=sink_node_id,
-            send_back_multiaddr=Multiaddr(
-                address=f"/ip4/169.254.0.{ip_counter}/tcp/{send_back_port}"
-            ),
-            connection_profile=ConnectionProfile(
-                throughput=1000, latency=1000, jitter=1000
-            ),
-        )
-
-    return _create_connection
