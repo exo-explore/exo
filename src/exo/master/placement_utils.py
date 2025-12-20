f37028747d079b282e801e153e1f26f0cc4a5413
Evan <evanev7@gmail.com>
2025-12-20 12:44:22 +0000

    all mastet tests pass

--- src/exo/master/placement_utils.py
+++ src/exo/master/placement_utils.py
@@ -268,15 +268,12 @@ def get_mlx_jaccl_devices_matrix(
             for conn in cycle_digraph.get_all_connections_between(node_i, node_j):
                 if isinstance(conn, RDMAConnection):
                     matrix[i][j] = conn.source_rdma_iface
-                break
+                    break
             else:
                 logger.warning(
                     f"Failed to find interface name between {node_i} and {node_j}"
                 )
-                raise ValueError(
-                    "Current ibv backend requires all-to-all rdma connections"
-                )
-
+                raise ValueError("Current jaccl backend requires all-to-all RDMA connections")
 
     return matrix
 
@@ -416,7 +413,7 @@ def get_mlx_jaccl_coordinators(
         logger.warning(
             f"Failed to find directly connected ip between {n} and {coordinator}"
         )
-        raise ValueError("Current jaccl backend requires all-to-all rdma connections")
+        raise ValueError("Current jaccl backend requires all participating devices to be able to communicate")
 
     return {
         n: f"{get_ip_for_node(n)}:{coordinator_port}" for n in selected_cycle
