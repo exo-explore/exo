89b0faf8a4345d1e74c5ccd75a44c8c1eb529300
ciaranbor <ciaranborourke-dev@proton.me>
2026-01-05 11:31:45 +0000

    Add initial QwenModelAdapter

---
+++ src/exo/worker/engines/image/models/qwen/__init__.py
@@ -0,0 +1,7 @@
+from exo.worker.engines.image.models.qwen.adapter import QwenModelAdapter
+from exo.worker.engines.image.models.qwen.config import QWEN_IMAGE_CONFIG
+
+__all__ = [
+    "QwenModelAdapter",
+    "QWEN_IMAGE_CONFIG",
+]
