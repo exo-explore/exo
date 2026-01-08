fc534e3a0d366dc4dd308ef57e89c948ccf62923
ciaranbor <ciaranborourke-dev@proton.me>
2026-01-08 12:23:31 +0000

    Chunk image input

--- src/exo/worker/runner/runner.py
+++ src/exo/worker/runner/runner.py
@@ -322,7 +322,6 @@ def main(
                                     for chunk_index, chunk_data in enumerate(
                                         data_chunks
                                     ):
-                                        is_last_chunk = chunk_index == total_chunks - 1
                                         event_sender.send(
                                             ChunkGenerated(
                                                 command_id=command_id,
@@ -339,11 +338,6 @@ def main(
 
                     current_status = RunnerReady()
                     logger.info("runner ready")
-                    event_sender.send(
-                        RunnerStatusUpdated(
-                            runner_id=runner_id, runner_status=RunnerReady()
-                        )
-                    )
                 case Shutdown():
                     current_status = RunnerShuttingDown()
                     logger.info("runner shutting down")
