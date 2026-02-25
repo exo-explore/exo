9f274b25bdff1d5a1d7ae6fa851d3a08921a188b
Evan <evanev7@gmail.com>
2026-02-25 16:43:41 +0000

    logloglog

--- src/exo/worker/engines/mlx/generator/generate.py
+++ src/exo/worker/engines/mlx/generator/generate.py
@@ -1,3 +1,4 @@
+import math
 import time
 from copy import deepcopy
 from typing import Callable, Generator, cast, get_args
@@ -248,6 +249,9 @@ def extract_top_logprobs(
     for i in range(top_logprobs):
         token_id = int(top_indices[i].item())
         token_logprob = float(top_values[i].item())
+        if math.isnan(token_logprob):
+            continue
+
         # Decode token ID to string
         token_str = tokenizer.decode([token_id])
         # Get byte representation
