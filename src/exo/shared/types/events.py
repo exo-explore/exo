cefac9539d3c6cdf9a959df2c0f261a8185e1c67
ciaranbor <ciaranborourke-dev@proton.me>
2026-03-06 18:24:39 +0000

    Add StorageConfigUpdated event

--- src/exo/shared/types/events.py
+++ src/exo/shared/types/events.py
@@ -7,6 +7,7 @@ from exo.shared.models.model_cards import ModelCard
 from exo.shared.topology import Connection
 from exo.shared.types.chunks import GenerationChunk, InputImageChunk
 from exo.shared.types.common import CommandId, Id, ModelId, NodeId, SessionId, SystemId
+from exo.shared.types.storage import StorageConfig
 from exo.shared.types.tasks import Task, TaskId, TaskStatus
 from exo.shared.types.worker.downloads import DownloadProgress
 from exo.shared.types.worker.instances import Instance, InstanceId
@@ -137,6 +138,12 @@ class TracesMerged(BaseEvent):
     traces: list[TraceEventData]
 
 
+@final
+class StorageConfigUpdated(BaseEvent):
+    node_id: NodeId
+    storage_config: StorageConfig
+
+
 Event = (
     TestEvent
     | TaskCreated
@@ -158,6 +165,7 @@ Event = (
     | TracesMerged
     | CustomModelCardAdded
     | CustomModelCardDeleted
+    | StorageConfigUpdated
 )
 
 
