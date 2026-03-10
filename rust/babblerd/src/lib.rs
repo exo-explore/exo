d411d08d1de734e9c2d2e02a02c294854c4be0b6
Evan <evanev7@gmail.com>
2026-03-10 13:16:09 +0000

    clippy

--- rust/babblerd/src/lib.rs
+++ rust/babblerd/src/lib.rs
@@ -1,25 +1,25 @@
-pub use error::{Error, Result};
+pub use error::{BabbleError, Result};
 pub use if_watcher::watch;
 pub mod error {
     use std::io;
 
-    pub type Result<T> = core::result::Result<T, Error>;
+    pub type Result<T> = core::result::Result<T, BabbleError>;
     #[derive(Debug)]
-    pub enum Error {
+    pub enum BabbleError {
         Io(io::Error),
         Unspecified,
         BabeldCrashed(Option<i32>),
         FailedToSetIp,
         Other(String),
     }
-    impl std::fmt::Display for Error {
+    impl std::fmt::Display for BabbleError {
         // use the debug display for now
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "{:?}", self)
+            write!(f, "{self:?}")
         }
     }
-    impl std::error::Error for Error {}
-    impl From<io::Error> for Error {
+    impl std::error::Error for BabbleError {}
+    impl From<io::Error> for BabbleError {
         fn from(value: io::Error) -> Self {
             Self::Io(value)
         }
@@ -37,7 +37,7 @@ pub mod if_watcher {
     use crate::{
         babel::Babble,
         ip_manager::add_ip,
-        {Error, Result},
+        {BabbleError, Result},
     };
 
     pub const PREFIX: Ipv6Net = Ipv6Net::new_assert(
@@ -52,7 +52,7 @@ pub mod if_watcher {
         tracing::info!("starting interface monitor");
         let mon = netwatch::netmon::Monitor::new()
             .await
-            .map_err(|_| Error::Unspecified)?;
+            .map_err(|_| BabbleError::Unspecified)?;
         let mut mon_stream = mon.interface_state().stream();
 
         while let Some(s) = mon_stream.next().await {
@@ -115,14 +115,14 @@ pub mod if_watcher {
                 }
 
                 tracing::info!("telling babeld to watch {}", iface.name());
-                let Ok(()) = send.send(Babble::AddIface(iface.name().to_string())).await else {
+                let Ok(()) = send.send(Babble::AddIface(iface.name().to_owned())).await else {
                     return Ok(());
                 };
-                ready_ifaces.insert(iface.name().to_string());
+                ready_ifaces.insert(iface.name().to_owned());
             }
             for iface in not_seen {
                 tracing::info!("telling babeld to stop watching {iface}");
-                let Ok(()) = send.send(Babble::RemoveIface(iface.to_string())).await else {
+                let Ok(()) = send.send(Babble::RemoveIface(iface.clone())).await else {
                     return Ok(());
                 };
                 assert!(ready_ifaces.remove(&iface));
@@ -137,11 +137,11 @@ pub mod babel {
     #[tracing::instrument(skip(sock, receiver))]
     pub async fn handle_listener(sock: UnixStream, mut receiver: broadcast::Receiver<String>) {
         tracing::info!("new socket conn");
-        let (read, mut write) = sock.into_split();
-        let mut read = BufReader::new(read).lines();
+        let (reader, mut write) = sock.into_split();
+        let mut reader = BufReader::new(reader).lines();
         loop {
             tokio::select! {
-                read = read.next_line() => {
+                read = reader.next_line() => {
                     let Ok(Some(_)) = read else { break; };
                 }
                 recv = receiver.recv() => {
@@ -169,7 +169,7 @@ pub mod babel {
     use tokio::process::Command;
     use tokio::sync::{broadcast, mpsc};
 
-    use crate::{Error, Result, if_watcher::PREFIX};
+    use crate::{BabbleError, Result, if_watcher::PREFIX};
 
     #[derive(Debug)]
     pub enum Babble {
@@ -195,7 +195,7 @@ pub mod babel {
                 .arg("-G")
                 .arg(PRIVATE_SOCK_PATH)
                 .arg("-C")
-                .arg(format!("redistribute ip {} local allow\n", PREFIX))
+                .arg(format!("redistribute ip {PREFIX} local allow\n"))
                 .arg("-C")
                 .arg("redistribute local deny\n")
                 .arg(first_iface)
@@ -220,7 +220,7 @@ pub mod babel {
                 let Some(line) = read.next_line().await? else {
                     return Ok(None);
                 };
-                tracing::info!("[babel] {}", line);
+                tracing::info!("[babel] {:?}", line);
                 if line == "ok" {
                     return Ok(Some(true));
                 } else if line == "bad" {
@@ -230,19 +230,14 @@ pub mod babel {
         }
 
         async fn supervise(
-            &mut self,
+            &self,
             mut recv: mpsc::Receiver<Babble>,
             send: broadcast::Sender<String>,
         ) -> Result<()> {
-            loop {
-                tokio::time::sleep(Duration::from_millis(10)).await;
-                match tokio::fs::try_exists(PRIVATE_SOCK_PATH).await {
-                    Ok(true) => break,
-                    _ => {
-                        tracing::info!("where is the sock");
-                        tokio::time::sleep(Duration::from_secs(1)).await;
-                    }
-                }
+            tokio::time::sleep(Duration::from_millis(10)).await;
+            while !matches!(tokio::fs::try_exists(PRIVATE_SOCK_PATH).await, Ok(true)) {
+                tracing::info!("where is the sock");
+                tokio::time::sleep(Duration::from_secs(1)).await;
             }
             tracing::debug!("connecting to babeld.sock");
             let (reader, mut writer) = UnixStream::connect(PRIVATE_SOCK_PATH).await?.into_split();
@@ -265,10 +260,10 @@ pub mod babel {
                         };
                         match babble {
                             Babble::AddIface(iface) => {
-                                Self::query(&mut babel_lines, &mut writer, format!("interface {}\n", iface).as_ref()).await?;
+                                Self::query(&mut babel_lines, &mut writer, format!("interface {iface}\n").as_ref()).await?;
                             }
                             Babble::RemoveIface(iface) => {
-                                Self::query(&mut babel_lines, &mut writer, format!("flush interface {}\n", iface).as_ref()).await?;
+                                Self::query(&mut babel_lines, &mut writer, format!("flush interface {iface}\n").as_ref()).await?;
                             }
                         }
                     },
@@ -288,7 +283,8 @@ pub mod babel {
 
         async fn shutdown(mut self) -> Result<()> {
             if let Some(pid) = self.proc.id() {
-                let pid = pid as libc::pid_t;
+                let pid: libc::pid_t = pid.try_into().expect("pid overflow");
+                // SAFETY: pid >= 0, freshly checked.
                 let rc = unsafe { libc::kill(pid, libc::SIGINT) };
                 if rc != 0 && rc != libc::ESRCH {
                     return Err(io::Error::last_os_error().into());
@@ -300,17 +296,17 @@ pub mod babel {
                     })
                     .await;
                 match exit_code {
-                    Some(exit_code) => {
-                        let code = exit_code?;
+                    Some(Ok(code)) => {
                         if code.success() {
                             Ok(())
                         } else {
-                            Err(Error::BabeldCrashed(code.code()))
+                            Err(BabbleError::BabeldCrashed(code.code()))
                         }
                     }
+                    Some(Err(e)) => Err(e.into()),
                     None => {
                         self.proc.kill().await?;
-                        Err(Error::BabeldCrashed(None))
+                        Err(BabbleError::BabeldCrashed(None))
                     }
                 }
             } else {
@@ -330,41 +326,41 @@ pub mod babel {
                     break iface;
                 }
                 Some(Babble::RemoveIface(iface)) => {
-                    tracing::warn!("ignored erroneous Babble::RemoveIface for {iface}")
+                    tracing::warn!("ignored erroneous Babble::RemoveIface for {iface}");
                 }
                 None => return Ok(()),
             }
         };
 
-        let mut babel = BabeldProcess::spawn(first_iface)?;
+        let babel = BabeldProcess::spawn(first_iface)?;
         let ret = babel.supervise(recv, send).await;
         _ = babel.shutdown().await;
         ret
     }
 }
-mod ip_manager {
+pub(crate) mod ip_manager {
     pub(crate) use sys::add_ip;
 
     #[cfg(target_os = "linux")]
     mod sys {
         use netwatch::interfaces::Interface;
 
-        use crate::{Error, Result};
+        use crate::{BabbleError, Result};
         use std::{net::Ipv6Addr, process::Command};
 
         #[tracing::instrument]
-        pub(crate) fn add_ip(v6: Ipv6Addr, iface: &Interface) -> Result<()> {
+        pub fn add_ip(v6: Ipv6Addr, iface: &Interface) -> Result<()> {
             let out = Command::new("ip")
                 .arg("addr")
                 .arg("add")
-                .arg(format!("{}/128", v6))
+                .arg(format!("{v6}/128"))
                 .arg("dev")
                 .arg(iface.name())
                 .output()?;
             if out.status.success() {
                 Ok(())
             } else {
-                Err(Error::FailedToSetIp)
+                Err(BabbleError::FailedToSetIp)
             }
         }
     }
@@ -373,22 +369,22 @@ mod ip_manager {
     mod sys {
         use netwatch::interfaces::Interface;
 
-        use crate::Error;
+        use crate::BabbleError;
         use crate::Result;
         use std::{net::Ipv6Addr, process::Command};
 
         #[tracing::instrument]
-        pub(crate) fn add_ip(v6: Ipv6Addr, iface: &Interface) -> Result<()> {
+        pub fn add_ip(v6: Ipv6Addr, iface: &Interface) -> Result<()> {
             let out = Command::new("ifconfig")
                 .arg(iface.name())
                 .arg("inet6")
-                .arg(format!("{}/128", v6.to_string()))
+                .arg(format!("{v6}/128"))
                 .arg("add")
                 .output()?;
             if out.status.success() {
                 Ok(())
             } else {
-                Err(Error::FailedToSetIp)
+                Err(BabbleError::FailedToSetIp)
             }
         }
     }
