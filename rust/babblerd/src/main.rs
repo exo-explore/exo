f7a4e7b7e86731e97b375363950fe814e7af1988
Evan <evanev7@gmail.com>
2026-03-11 14:32:10 +0000

    move private directories

--- rust/babblerd/src/main.rs
+++ rust/babblerd/src/main.rs
@@ -10,13 +10,13 @@ use tokio::{
 };
 
 #[cfg(target_os = "macos")]
-const PUBLIC_DIR: &str = "/var/run/exo";
+const PUBLIC_DIR: &str = "/var/run/babbler";
 #[cfg(target_os = "linux")]
-const PUBLIC_DIR: &str = "/run/exo";
+const PUBLIC_DIR: &str = "/run/babbler";
 #[cfg(target_os = "macos")]
-const PUBLIC_SOCK_PATH: &str = "/var/run/babblerd.sock";
+const PUBLIC_SOCK_PATH: &str = "/var/run/babbler/babblerd.sock";
 #[cfg(target_os = "linux")]
-const PUBLIC_SOCK_PATH: &str = "/run/babblerd.sock";
+const PUBLIC_SOCK_PATH: &str = "/run/babbler/babblerd.sock";
 
 enum State {
     Idle,
@@ -50,7 +50,7 @@ async fn main() -> color_eyre::Result<()> {
     }
     std::fs::create_dir_all(PUBLIC_DIR)?;
     // make our directory world readable
-    std::fs::set_permissions(PUBLIC_DIR, Permissions::from_mode(0o0777))?;
+    std::fs::set_permissions(PUBLIC_DIR, Permissions::from_mode(0o0755))?;
     let res = inner_main().await;
     _ = std::fs::remove_file(PUBLIC_SOCK_PATH);
     res
@@ -59,6 +59,8 @@ async fn main() -> color_eyre::Result<()> {
 async fn inner_main() -> color_eyre::Result<()> {
     tracing::info!("creating socket at {PUBLIC_SOCK_PATH}");
     let public_socket = UnixListener::bind(PUBLIC_SOCK_PATH)?;
+    // make our socket world accessible
+    std::fs::set_permissions(PUBLIC_SOCK_PATH, Permissions::from_mode(0o0666))?;
     let mut babbler: State = State::Idle;
 
     loop {
@@ -97,9 +99,9 @@ async fn inner_main() -> color_eyre::Result<()> {
                         if let Ok(e) = watcher.await {
                             e.wrap_err("while ctrl-c")?
                         }
-                        babel.await??;
+                        babel.await?.wrap_err("while ctrl-c")?;
                         while let Some(res) = listeners.join_next().await {
-                            res?;
+                            res.wrap_err("while ctrl-c")?;
                         }
                         break;
                     }
