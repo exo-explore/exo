b6240a97e896fe09e8b402601d2ef2683e4d0de4
rltakashige <rl.takashige@gmail.com>
2026-03-24 17:16:41 +0000

    Prevent Qwen3.5 looping by using mlx lm fork (#1784)
    ## Motivation
    
    Move back to an MLX LM to improve the Qwen 3.5 experience. 
    
    ## Test Plan
    
    ### Manual Testing
    Seems to loop less from testing, no speed regressions.

--- src/exo/worker/engines/mlx/patches/__init__.py
+++ src/exo/worker/engines/mlx/patches/__init__.py
@@ -1,6 +1,3 @@
-from exo.worker.engines.mlx.patches.high_precision_gdn_softplus import (
-    patch_gdn_softplus,
-)
 from exo.worker.engines.mlx.patches.opt_batch_gen import apply_batch_gen_patch
 from exo.worker.engines.mlx.patches.standard_yarn_rope import patch_yarn_rope
 
@@ -13,5 +10,5 @@ def apply_mlx_patches() -> None:
         return
     _applied = True
     patch_yarn_rope()
-    patch_gdn_softplus()
+    # patch_gdn_softplus()
     apply_batch_gen_patch()
