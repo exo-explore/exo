09e894dd5288465c9aa8b7c8cfaa4a7f73859ade
rltakashige <rl.takashige@gmail.com>
2026-04-19 12:55:20 +0100

    Fix vision models on M5 Pro/Max MacBooks (#1927)
    ## Motivation
    
    Vision models don't understand images on M5 series MacBooks. The
    upstream NAX addmm fix (https://github.com/ml-explore/mlx/pull/3422)
    fixes this.
    
    ## Why It Works
    Same conclusion I came to when I was debugging the issue on an M5 Max.
    It works after this fix.
    
    ## Test Plan
    
    ### Manual Testing
    Works for Qwen3.5 27B

--- nix/mlx.nix
+++ nix/mlx.nix
@@ -49,7 +49,7 @@ let
       owner = "rltakashige";
       repo = "mlx-jaccl-fix-small-recv";
       rev = uvLockMlxRev;
-      hash = "sha256-F047XI9dsWLfFqAzddSHJeLkIiEsCKc8n9fF70uShfA=";
+      hash = "sha256-1/utA5CMmQWjYDgWcpnqPVPc/T7sqNVL+Hwv3tcy/ng=";
     };
 
     patches = [
