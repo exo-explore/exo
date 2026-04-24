2d945bc7fbbd801bcad91f9430b91c3e77251aa3
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2026-04-24 14:37:20 +0100

    run test

--- mlx_tinygrad_interop/my_interop.py
+++ mlx_tinygrad_interop/my_interop.py
@@ -1,19 +1,30 @@
+from tinygrad.helpers import GlobalCounters
 from tinygrad.tensor import Tensor
 from tinygrad.device import Device
 from tinygrad.dtype import _from_torch_dtype, _from_np_dtype
 import torch
+import time
+import statistics
+
+def pytorch_to_tinygrad(tensor: torch.Tensor) -> Tensor:
+    return Tensor.from_blob(tensor.data_ptr(), tensor.shape, dtype=_from_torch_dtype(tensor.dtype), device="METAL")
 
 def main() -> None:
-    tensor1 = torch.tensor([1.0, 2.0, 3.0], device=torch.device("mps"))
-    tiny_tensor1 = Tensor.from_blob(tensor1.data_ptr(), tensor1.shape, dtype=_from_torch_dtype(tensor1.dtype), device='METAL')
-    
-    # Before tinygrad calculations, mps needs to be synchronized to make sure data is valid.
-    if tensor1.device.type == "mps": torch.mps.synchronize()
-    else: torch.cuda.synchronize()
-    
-    x = (tiny_tensor1 + 1).realize()
-    
-    print(x)
+    for i in range(8):
+        N = 256 * (4 ** i)
+        x = torch.zeros(N, device=torch.device("mps"))
+        
+        vals = []
+        for j in range(1000):
+            x = x.uniform_()
+            torch.mps.synchronize()
+
+            old = time.perf_counter_ns()
+            pytorch_to_tinygrad(x)
+            new = time.perf_counter_ns()
+            vals.append(new - old)
+        print( f"result: {N*4:8d} pytorch to tinygrad in {statistics.mean(vals):.2f}ns with stddev {statistics.stdev(vals):.2f}ns" )
+
 
 if __name__=="__main__":
     main()
