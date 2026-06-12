b6b9761d5fc8d9ce7adb649b686fa5d6ae34992e
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2026-06-12 18:33:08 +0100

    sample implenentation of CliPy a newtype wrapper around python object references that is supposed to replace Py<T> such that it is parse-able from the CLI and Serde - and hence we can have NORMAL getters/setters without weird copy behaviour

--- rust/exo_rs/src/config/cli.rs
+++ rust/exo_rs/src/config/cli.rs
@@ -5,7 +5,7 @@ use crate::{pickle_reduce, version};
 use clap::{ArgAction, Parser};
 use pyo3::prelude::{PyAnyMethods, PyModuleMethods};
 use pyo3::types::{PyModule, PyTuple};
-use pyo3::{Bound, PyAny, PyResult, Python, pyclass, pymethods};
+use pyo3::{pyclass, pymethods, Bound, PyAny, PyResult, Python};
 use pyo3_stub_gen::derive::{gen_stub_pyclass, gen_stub_pymethods};
 use serde::{Deserialize, Serialize};
 use std::ffi::OsString;
@@ -207,6 +207,224 @@ impl DeprecatedArgs {
         }
     }
 }
+
+// pub mod cli_py {
+//     use crate::ext::ResultExt;
+//     use clap::{ArgMatches, Args, CommandFactory, FromArgMatches, Parser, Subcommand};
+//     use pyo3::pyclass::boolean_struct::False;
+//     use pyo3::{Py, PyClass, PyClassInitializer, PyErr, PyResult, Python};
+//     use std::ffi::OsString;
+//     use std::fmt::{Debug, Display, Formatter};
+//     use std::ops::{Deref, DerefMut};
+//     use std::str::FromStr;
+//
+//     #[repr(transparent)]
+//     pub struct CliPy<T>(Py<T>);
+//
+//     impl<T: Debug> Debug for CliPy<T>
+//     where
+//         T: PyClass,
+//     {
+//         #[inline(always)]
+//         fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//             Python::attach(|py| (&*self.borrow(py)).fmt(f))
+//         }
+//     }
+//
+//     impl<T: Clone> Clone for CliPy<T> {
+//         #[inline(always)]
+//         fn clone(&self) -> Self {
+//             Self::new(Python::attach(|py| self.clone_ref(py)))
+//         }
+//     }
+//
+//     impl<T: PartialEq> PartialEq for CliPy<T>
+//     where
+//         T: PyClass,
+//     {
+//         #[inline(always)]
+//         fn eq(&self, other: &Self) -> bool {
+//             Python::attach(|py| &*self.borrow(py) == &*other.borrow(py))
+//         }
+//     }
+//
+//     impl<T: Eq> Eq for CliPy<T> where T: PyClass {}
+//
+//     impl<T: Display> Display for CliPy<T>
+//     where
+//         T: PyClass,
+//     {
+//         #[inline(always)]
+//         fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//             Python::attach(|py| (&*self.borrow(py)).fmt(f))
+//         }
+//     }
+//
+//     impl<T: FromStr> FromStr for CliPy<T>
+//     where
+//         T: PyClass + Into<PyClassInitializer<T>>,
+//         T::Err: ToString,
+//     {
+//         type Err = PyErr;
+//
+//         #[inline(always)]
+//         fn from_str(s: &str) -> Result<Self, Self::Err> {
+//             Self::py_try_new(<T as FromStr>::from_str(s).pyerr()?)
+//         }
+//     }
+//
+//     impl<T> From<Py<T>> for CliPy<T> {
+//         #[inline(always)]
+//         fn from(inner: Py<T>) -> Self {
+//             Self::new(inner)
+//         }
+//     }
+//
+//     impl<T> Deref for CliPy<T> {
+//         type Target = Py<T>;
+//         #[inline(always)]
+//         fn deref(&self) -> &Self::Target {
+//             self.inner()
+//         }
+//     }
+//
+//     impl<T> DerefMut for CliPy<T> {
+//         #[inline(always)]
+//         fn deref_mut(&mut self) -> &mut Self::Target {
+//             self.inner_mut()
+//         }
+//     }
+//
+//     impl<T> CliPy<T> {
+//         #[inline(always)]
+//         fn new(inner: impl Into<Py<T>>) -> Self {
+//             Self(inner.into())
+//         }
+//
+//         #[inline(always)]
+//         pub fn py_try_new_with(
+//             py: Python<'_>,
+//             value: impl Into<PyClassInitializer<T>>,
+//         ) -> PyResult<Self>
+//         where
+//             T: PyClass,
+//         {
+//             Py::new(py, value).map(Self)
+//         }
+//
+//         #[inline(always)]
+//         pub fn py_try_new(value: impl Into<PyClassInitializer<T>>) -> PyResult<Self>
+//         where
+//             T: PyClass,
+//         {
+//             Python::attach(|py| Self::py_try_new_with(py, value))
+//         }
+//
+//         #[inline(always)]
+//         pub fn clap_try_new(value: impl Into<PyClassInitializer<T>>) -> Result<Self, clap::Error>
+//         where
+//             T: PyClass,
+//         {
+//             Self::py_try_new(value).map_err(|e| clap::Error::raw(clap::error::ErrorKind::Io, e))
+//         }
+//
+//         #[inline(always)]
+//         pub fn inner(&self) -> &Py<T> {
+//             &self.0
+//         }
+//
+//         #[inline(always)]
+//         pub fn inner_mut(&mut self) -> &mut Py<T> {
+//             &mut self.0
+//         }
+//
+//         #[inline(always)]
+//         pub fn into_inner(self) -> Py<T> {
+//             self.0
+//         }
+//     }
+//
+//     impl<T: Parser> Parser for CliPy<T>
+//     where
+//         T: PyClass<Frozen = False> + Into<PyClassInitializer<T>>,
+//     {
+//         fn try_parse() -> Result<Self, clap::Error> {
+//             <T as Parser>::try_parse().and_then(Self::clap_try_new)
+//         }
+//
+//         fn try_parse_from<I, It>(itr: I) -> Result<Self, clap::Error>
+//         where
+//             I: IntoIterator<Item = It>,
+//             It: Into<OsString> + Clone,
+//         {
+//             <T as Parser>::try_parse_from(itr).and_then(Self::clap_try_new)
+//         }
+//     }
+//
+//     impl<T: CommandFactory> CommandFactory for CliPy<T> {
+//         fn command() -> clap::Command {
+//             <T as CommandFactory>::command()
+//         }
+//         fn command_for_update() -> clap::Command {
+//             <T as CommandFactory>::command_for_update()
+//         }
+//     }
+//
+//     impl<T: FromArgMatches> FromArgMatches for CliPy<T>
+//     where
+//         T: PyClass<Frozen = False> + Into<PyClassInitializer<T>>,
+//     {
+//         fn from_arg_matches(matches: &ArgMatches) -> Result<Self, clap::Error> {
+//             <T as FromArgMatches>::from_arg_matches(matches).and_then(Self::clap_try_new)
+//         }
+//         fn from_arg_matches_mut(matches: &mut ArgMatches) -> Result<Self, clap::Error> {
+//             <T as FromArgMatches>::from_arg_matches_mut(matches).and_then(Self::clap_try_new)
+//         }
+//         fn update_from_arg_matches(&mut self, matches: &ArgMatches) -> Result<(), clap::Error> {
+//             Python::attach(|py| {
+//                 <T as FromArgMatches>::update_from_arg_matches(&mut *self.borrow_mut(py), matches)
+//             })
+//         }
+//         fn update_from_arg_matches_mut(
+//             &mut self,
+//             matches: &mut ArgMatches,
+//         ) -> Result<(), clap::Error> {
+//             Python::attach(|py| {
+//                 <T as FromArgMatches>::update_from_arg_matches_mut(
+//                     &mut *self.borrow_mut(py),
+//                     matches,
+//                 )
+//             })
+//         }
+//     }
+//
+//     impl<T: Args> Args for CliPy<T>
+//     where
+//         T: PyClass<Frozen = False> + Into<PyClassInitializer<T>>,
+//     {
+//         fn augment_args(cmd: clap::Command) -> clap::Command {
+//             <T as Args>::augment_args(cmd)
+//         }
+//         fn augment_args_for_update(cmd: clap::Command) -> clap::Command {
+//             <T as Args>::augment_args_for_update(cmd)
+//         }
+//     }
+//
+//     impl<T: Subcommand> Subcommand for CliPy<T>
+//     where
+//         T: PyClass<Frozen = False> + Into<PyClassInitializer<T>>,
+//     {
+//         fn augment_subcommands(cmd: clap::Command) -> clap::Command {
+//             <T as Subcommand>::augment_subcommands(cmd)
+//         }
+//         fn augment_subcommands_for_update(cmd: clap::Command) -> clap::Command {
+//             <T as Subcommand>::augment_subcommands_for_update(cmd)
+//         }
+//         fn has_subcommand(name: &str) -> bool {
+//             <T as Subcommand>::has_subcommand(name)
+//         }
+//     }
+// }
 
 pub fn cli_submodule(m: &Bound<PyModule>) -> PyResult<()> {
     m.add_class::<CliArgs>()?;
