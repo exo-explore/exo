9e1a41b687b794975dc0c71d0053912f810a88f6
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2026-06-13 19:49:54 +0100

    re-org constants

--- src/exo/shared/constants.py
+++ src/exo/shared/constants.py
@@ -3,7 +3,11 @@ from pathlib import Path
 
 from exo.utils.dashboard_path import find_dashboard, find_resources
 
-# NOTE: I will leave these alone here for now - I don't wanna port them to Rust yet
+# NOTE: I will leave these alone here for now - I don't know how I should port raw constants
+EXO_MAX_CHUNK_SIZE = 512 * 1024
+EXO_MAX_INSTANCE_RETRIES = 5
+
+# NOTE: I will leave these alone here for now - I don't wanna (or know how to) port them to Rust yet
 _RESOURCES_DIR_ENV = os.environ.get("EXO_RESOURCES_DIR", None)
 RESOURCES_DIR = (
     find_resources() if _RESOURCES_DIR_ENV is None else Path.home() / _RESOURCES_DIR_ENV
@@ -12,9 +16,3 @@ _DASHBOARD_DIR_ENV = os.environ.get("EXO_DASHBOARD_DIR", None)
 DASHBOARD_DIR = (
     find_dashboard() if _DASHBOARD_DIR_ENV is None else Path.home() / _DASHBOARD_DIR_ENV
 )
-
-EXO_MAX_CHUNK_SIZE = (
-    512 * 1024
-)  # NOTE: I will leave these alone here for now - I don't know how I should port raw constants
-
-EXO_MAX_INSTANCE_RETRIES = 5
